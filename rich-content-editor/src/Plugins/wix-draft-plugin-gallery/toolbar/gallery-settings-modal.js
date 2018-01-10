a6de16c5fc9eb91ff9739a0c8c485dbcd7a76ee8
lxgreen <alexgr@wix.com>
2018-01-10 14:06:12 +0200

    [rich-content-editor] feat(gallery-settings): wip

--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/gallery-settings-modal.js
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/gallery-settings-modal.js
@@ -1,10 +1,8 @@
+import React, { Component } from 'react';
+
 export class GallerySettingsModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
   render() {
-    return ( <div style={{border: '2px red solid', padding: '10px'}}>Gallery Settings</div> )
+    return <div style={{ border: '2px red solid', padding: '10px' }}>Gallery Settings</div>;
   }
 }
 
