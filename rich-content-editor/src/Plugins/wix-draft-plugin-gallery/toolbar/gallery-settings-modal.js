e338ec4621a938e91d894876ff7efebaf2f6ecb7
lxgreen <alexgr@wix.com>
2018-01-10 13:22:52 +0200

    [rich-content-editor] feat(gallery-settings): implementation init

---
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/gallery-settings-modal.js
@@ -0,0 +1,11 @@
+export class GallerySettingsModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return ( <div style={{border: '2px red solid', padding: '10px'}}>Gallery Settings</div> )
+  }
+}
+
+export default GallerySettingsModal;
--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/inline-buttons.jsx
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/inline-buttons.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AddIcon from '../icons/toolbar/icon-add.svg';
+import SettingsIcon from '../../base/icons/block-settings.svg';
 import GridSmallIcon from '../icons/toolbar/layout/icon-grid-small.svg';
 import GridSmallIconActive from '../icons/toolbar/layout/icon-grid-small-active.svg';
 import GridMediumIcon from '../icons/toolbar/layout/icon-grid-medium.svg';
 import GridMediumIconActive from '../icons/toolbar/layout/icon-grid-medium-active.svg';
 import GridLargeIcon from '../icons/toolbar/layout/icon-grid-large.svg';
 import GridLargeIconActive from '../icons/toolbar/layout/icon-grid-large-active.svg';
+import GallerySettingsModal from './gallery-settings-modal';
 import { BUTTONS } from '~/Plugins/base/buttons';
 
 class LayoutModal extends React.Component {
@@ -104,6 +106,14 @@ const InlineButtons = [
   { type: BUTTONS.SIZE_SMALL_RIGHT },
   { type: BUTTONS.SIZE_CONTENT },
   { type: BUTTONS.SIZE_FULL_WIDTH },
+  { type: BUTTONS.SEPARATOR },
+  {
+    keyName: 'gallery_settings',
+    type: BUTTONS.EXTERNAL_MODAL,
+    icon: SettingsIcon,
+    panelElement: GallerySettingsModal,
+    onClick: pubsub => console.log('*** click gallery settings *** '),
+  },
   { type: BUTTONS.SEPARATOR },
   { type: BUTTONS.DELETE },
 ];
