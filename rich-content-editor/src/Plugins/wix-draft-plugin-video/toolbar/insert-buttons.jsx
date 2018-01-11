8d594e6d9ae3e22d71aded02f1d10dd0084887c6
Shaul Goral <shaulg@wix.com>
2018-01-11 16:20:30 +0200

    feature (videoUploadModal)

--- rich-content-editor/src/Plugins/wix-draft-plugin-video/toolbar/insert-buttons.jsx
+++ rich-content-editor/src/Plugins/wix-draft-plugin-video/toolbar/insert-buttons.jsx
@@ -4,6 +4,7 @@ import InsertPluginIcon from '../icons/insert-plugin.svg';
 export default ({ helpers }) => {
   return [
     {
+      type: 'video',
       name: 'Video',
       tooltipText: 'Add a Video',
       Icon: InsertPluginIcon,
