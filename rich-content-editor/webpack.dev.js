f60d5e6576d798c9de789bb11eae274217db8994
Jonathan Bachman <jonathanb@wix.com>
2018-01-11 14:42:46 +0200

    [rce] infra(webpack): webpack-bundle-analyzer

--- rich-content-editor/webpack.dev.js
+++ rich-content-editor/webpack.dev.js
@@ -1,8 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-//const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = merge(common, {
   devtool: 'eval-source-map',
-  //plugins: [new Visualizer()],
 });
--- rich-content-editor/webpack.prod.js
+++ rich-content-editor/webpack.prod.js
@@ -1,7 +1,29 @@
+/* eslint-disable */
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = merge(common, {
-  plugins: [new UglifyJsPlugin()],
-});
+const prodConfig = {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+    }),
+    new UglifyJsPlugin({
+      parallel: true,
+      uglifyOptions: {
+        output: {
+          beautify: false,
+        },
+      },
+    }),
+  ],
+};
+
+module.exports = function(env) {
+  if (env && env.analyzeBundle) {
+    prodConfig.plugins.push(new BundleAnalyzerPlugin());
+  }
+  return merge(common, prodConfig);
+};
