2759fecb3f79fc47f9afe67654c871a3ea0d902b
Shaul Goral <shaulg@wix.com>
2018-01-16 11:54:38 +0200

    feature (video inline buttons)

--- rich-content-editor/src/Plugins/wix-draft-plugin-video/toolbar/inline-buttons.jsx
+++ rich-content-editor/src/Plugins/wix-draft-plugin-video/toolbar/inline-buttons.jsx
@@ -1,13 +1,21 @@
 import { BUTTONS } from '~/Plugins/base/buttons';
+import MediaReplaceIcon from '../icons/media-replace.svg';
 
 const InlineButtons = [
-  { type: BUTTONS.SIZE_SMALL_LEFT },
   { type: BUTTONS.SIZE_SMALL_CENTER },
-  { type: BUTTONS.SIZE_SMALL_RIGHT },
   { type: BUTTONS.SIZE_CONTENT },
   { type: BUTTONS.SIZE_FULL_WIDTH },
   { type: BUTTONS.SEPARATOR },
+  { type: BUTTONS.SIZE_SMALL_LEFT },
+  { type: BUTTONS.SIZE_SMALL_RIGHT },
+  { type: BUTTONS.SEPARATOR },
+  {
+    keyName: 'replace',
+    type: BUTTONS.VIDEO_REPLACE,
+    icon: MediaReplaceIcon,
+  },
   { type: BUTTONS.DELETE },
 ];
+
 
 export default InlineButtons;
