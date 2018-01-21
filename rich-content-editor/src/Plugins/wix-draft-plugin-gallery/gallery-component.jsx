5d871b754c19cac66ded426b2baf1a5d5e9307a9
Guy Sopher <guyso@wix.com>
2018-01-21 15:28:49 +0200

    [rce] feat(gallery-component): upload multiple items (w/o media servers)

--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/gallery-component.jsx
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/gallery-component.jsx
@@ -6,56 +6,56 @@ import 'pro-gallery-renderer/dist/statics/main.min.css';
 const EMPTY_SMALL_PLACEHOLDER = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 const DEFAULTS = {
-  items: [
-    {
-      metadata: {
-        height: 1000,
-        width: 1920,
-      },
-      itemId: '8b72558253b2502b401bb46e5599f22a',
-      url: '8bb438_1b73a6b067b24175bd087e86613bd00c.jpg',
-    },
-    {
-      metadata: {
-        height: 5600,
-        width: 3737,
-      },
-      itemId: '2d3b675ea857dc41158bad3b28300824',
-      url: '8bb438_78ff5e32500d48cdaa22a3f446d68216.jpg',
-    },
-    {
-      metadata: {
-        height: 3737,
-        width: 5600,
-      },
-      itemId: '860df034014674abd7a2e73abe0b851b',
-      url: '8bb438_ff062a651e174cf5926fe5c088be1099.jpg',
-    },
-    {
-      metadata: {
-        height: 333,
-        width: 500,
-      },
-      itemId: '8b72558253b2502b401bb46e5599f22b',
-      url: '8bb438_bc43580d2f36408f87cf8302a86e5ad4.jpg',
-    },
-    {
-      metadata: {
-        height: 560,
-        width: 374,
-      },
-      itemId: '2d3b675ea857dc41158bad3b28300825',
-      url: '8bb438_7dde2abf38e24163b80c11619c357bf1.jpg',
-    },
-    {
-      metadata: {
-        height: 374,
-        width: 560,
-      },
-      itemId: '860df034014674abd7a2e73abe0b851c',
-      url: '8bb438_ac190df97cfe4c6d8bcd9ca8b6b3c100.jpg',
-    },
-  ],
+  items: [],
+  //   {
+  //     metadata: {
+  //       height: 1000,
+  //       width: 1920,
+  //     },
+  //     itemId: '8b72558253b2502b401bb46e5599f22a',
+  //     url: '8bb438_1b73a6b067b24175bd087e86613bd00c.jpg',
+  //   },
+  //   {
+  //     metadata: {
+  //       height: 5600,
+  //       width: 3737,
+  //     },
+  //     itemId: '2d3b675ea857dc41158bad3b28300824',
+  //     url: '8bb438_78ff5e32500d48cdaa22a3f446d68216.jpg',
+  //   },
+  //   {
+  //     metadata: {
+  //       height: 3737,
+  //       width: 5600,
+  //     },
+  //     itemId: '860df034014674abd7a2e73abe0b851b',
+  //     url: '8bb438_ff062a651e174cf5926fe5c088be1099.jpg',
+  //   },
+  //   {
+  //     metadata: {
+  //       height: 333,
+  //       width: 500,
+  //     },
+  //     itemId: '8b72558253b2502b401bb46e5599f22b',
+  //     url: '8bb438_bc43580d2f36408f87cf8302a86e5ad4.jpg',
+  //   },
+  //   {
+  //     metadata: {
+  //       height: 560,
+  //       width: 374,
+  //     },
+  //     itemId: '2d3b675ea857dc41158bad3b28300825',
+  //     url: '8bb438_7dde2abf38e24163b80c11619c357bf1.jpg',
+  //   },
+  //   {
+  //     metadata: {
+  //       height: 374,
+  //       width: 560,
+  //     },
+  //     itemId: '860df034014674abd7a2e73abe0b851c',
+  //     url: '8bb438_ac190df97cfe4c6d8bcd9ca8b6b3c100.jpg',
+  //   },
+  // ],
   styles: {
     galleryLayout: 0, // OK
     oneRow: false, // scrollDirection OK?
@@ -94,31 +94,31 @@ class GalleryComponent extends React.Component {
     const styles = props.componentData.styles || DEFAULTS.styles;
     const layout = props.componentData.config && props.componentData.config.layout;
     const layoutWidth = layout === 'large' ? 100 : layout === 'medium' ? 50 : 33;
+    const isLoading = props.componentState.isLoading || 0;
 
     const state = {
       items,
       inEditMode,
       layoutWidth,
       styles,
+      isLoading
     };
 
-    const { alreadyLoading, isLoading, userSelectedFiles } = this.getLoadingParams(componentState);
-    if (!alreadyLoading) {
-      if (isLoading !== true && userSelectedFiles) {
-        //lets continue the uploading process
-        console.log('userSelectedFiles', userSelectedFiles); //eslint-disable-line no-console
-        if (userSelectedFiles.files && userSelectedFiles.files.length > 0) {
+    const { userSelectedFiles } = componentState;
+    if (isLoading <= 0 && userSelectedFiles) {
+      //lets continue the uploading process
+      if (userSelectedFiles.files && userSelectedFiles.files.length > 0) {
+        Object.assign(state, { isLoading: userSelectedFiles.files.length });
+        for (let file, i = 0; file = userSelectedFiles.files[i]; i++) {
           const reader = new FileReader();
-          reader.onload = e => this.fileLoaded(e, userSelectedFiles.files[0]);
-          reader.readAsDataURL(userSelectedFiles.files[0]);
-          console.log('2 userSelectedFiles', userSelectedFiles); //eslint-disable-line no-console
-          Object.assign(state, { isLoading: true, files: userSelectedFiles.files, dataUrl: EMPTY_SMALL_PLACEHOLDER });
+          reader.onload = e => this.fileLoaded(e, userSelectedFiles.files[i]);
+          reader.readAsDataURL(userSelectedFiles.files[i]);
         }
-        setTimeout(() => {
-          //needs to be async since this function is called during constructor and we do not want the update to call set state on other components
-          this.props.store.update('componentState', { isLoading: true, userSelectedFiles: null });
-        }, 0);
       }
+      setTimeout(() => {
+        //needs to be async since this function is called during constructor and we do not want the update to call set state on other components
+        this.props.store.update('componentState', { isLoading: true, userSelectedFiles: null });
+      }, 0);
     }
 
     return state;
@@ -126,37 +126,36 @@ class GalleryComponent extends React.Component {
 
   imageLoaded = event => {
     const img = event.target;
-    const { items } = this.state;
-    items.push({
+    let {items, isLoading} = this.state;
+    items = [...items, {
       metadata: {
         height: img.height,
         width: img.width,
       },
-      itemId: event.timestamp,
+      itemId: String(event.timeStamp),
       url: img.src,
-    });
-    this.setState({ isLoading: true, files: null, items });
+    }];
+    console.log('New items loaded', items);
+    this.setState({ isLoading: (isLoading - 1), items });
+    this.props.store.update('componentData', {items});
   }
 
-  fileLoaded = event => {
+  fileLoaded = (event, file) => {
 
     const img = new Image();
     img.onload = this.imageLoaded;
     img.src = event.target.result;
 
     // const files = Array.from(this.state.files);
-    // const { helpers } = this.props;
-    // const hasFileChangeHelper = helpers && helpers.onFilesChange;
-    // if (hasFileChangeHelper) {
-    //   helpers.onFilesChange(files, ({ data, error }) => {
-    //     const { setData } = this.props.blockProps;
-    //     this.props.componentData.item = data;
-    //     setData(this.props.componentData);
-    //     this.resetLoadingState(error);
-    //   });
-    // } else {
-    //   this.resetLoadingState({ msg: 'Missing upload function' });
-    // }
+    const { helpers } = this.props;
+    const hasFileChangeHelper = helpers && helpers.onFilesChange;
+    if (hasFileChangeHelper) {
+      helpers.onFilesChange(files, ({ data, error }) => {
+        console.log('onFilesChanged happend', data, error);
+      });
+    } else {
+      console.warn('Missing upload function');
+    }
   };
 
   getLoadingParams = componentState => {
