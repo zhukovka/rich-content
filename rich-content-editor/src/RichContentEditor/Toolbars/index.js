7f98c5337dd07d089546bf5981b66a37e632ebea
Jonathan Bachman <jonathanb@wix.com>
2018-01-25 16:44:07 +0200

    [rce] fix(plugins): disable text toolbar for mobile

--- rich-content-editor/src/RichContentEditor/Toolbars/index.js
+++ rich-content-editor/src/RichContentEditor/Toolbars/index.js
@@ -4,12 +4,12 @@ import { createTextToolbar } from './InlineToolbar';
 
 const createToolbars = ({ pluginButtons, textButtons, sideToolbarOffset, isMobile }) => {
   const toolbars = {
-    text: createTextToolbar({ buttons: textButtons }),
     side: createSideToolbar({ buttons: pluginButtons, isMobile, offset: sideToolbarOffset }),
     mobile: createMobileToolbar({ buttons: textButtons }),
   };
 
   if (!isMobile) {
+    toolbars.text = createTextToolbar({ buttons: textButtons });
     toolbars.static = createFooterToolbar({ buttons: pluginButtons });
   }
 
