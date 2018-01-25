535846d1cb2df7f78d263c24610f728dd2545ee6
Jonathan Bachman <jonathanb@wix.com>
2018-01-25 16:47:27 +0200

    [rce] fix(plugins): disable mobile toolbar for desktop

--- rich-content-editor/src/RichContentEditor/Toolbars/index.js
+++ rich-content-editor/src/RichContentEditor/Toolbars/index.js
@@ -4,13 +4,14 @@ import { createTextToolbar } from './InlineToolbar';
 
 const createToolbars = ({ pluginButtons, textButtons, sideToolbarOffset, isMobile }) => {
   const toolbars = {
-    side: createSideToolbar({ buttons: pluginButtons, isMobile, offset: sideToolbarOffset }),
-    mobile: createMobileToolbar({ buttons: textButtons }),
+    side: createSideToolbar({ buttons: pluginButtons, offset: sideToolbarOffset, isMobile })
   };
 
   if (!isMobile) {
     toolbars.text = createTextToolbar({ buttons: textButtons });
     toolbars.static = createFooterToolbar({ buttons: pluginButtons });
+  } else {
+    toolbars.mobile = createMobileToolbar({ buttons: textButtons });
   }
 
   return toolbars;
