f5702180d4a5b6a138aca4effbd9614058c8efd0
Jonathan Bachman <jonathanb@wix.com>
2018-01-29 14:50:24 +0200

    [rce] chore(gallery plugin): removed bundled pro-gallery-renderer main.min.css import

--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/gallery-component.jsx
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/gallery-component.jsx
@@ -3,87 +3,8 @@ import PropTypes from 'prop-types';
 import { GalleryViewer, getDefault } from './gallery-viewer';
 //import { baseUtils } from 'photography-client-lib/dist/src/utils/baseUtils'; [for dev only]
 
-//import 'pro-gallery-renderer/dist/statics/main.min.css';
-
 //eslint-disable-next-line no-unused-vars
 const EMPTY_SMALL_PLACEHOLDER = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-
-const DEFAULTS = {
-  items: [],
-  //   {
-  //     metadata: {
-  //       height: 1000,
-  //       width: 1920,
-  //     },
-  //     itemId: '8b72558253b2502b401bb46e5599f22a',
-  //     url: '8bb438_1b73a6b067b24175bd087e86613bd00c.jpg',
-  //   },
-  //   {
-  //     metadata: {
-  //       height: 5600,
-  //       width: 3737,
-  //     },
-  //     itemId: '2d3b675ea857dc41158bad3b28300824',
-  //     url: '8bb438_78ff5e32500d48cdaa22a3f446d68216.jpg',
-  //   },
-  //   {
-  //     metadata: {
-  //       height: 3737,
-  //       width: 5600,
-  //     },
-  //     itemId: '860df034014674abd7a2e73abe0b851b',
-  //     url: '8bb438_ff062a651e174cf5926fe5c088be1099.jpg',
-  //   },
-  //   {
-  //     metadata: {
-  //       height: 333,
-  //       width: 500,
-  //     },
-  //     itemId: '8b72558253b2502b401bb46e5599f22b',
-  //     url: '8bb438_bc43580d2f36408f87cf8302a86e5ad4.jpg',
-  //   },
-  //   {
-  //     metadata: {
-  //       height: 560,
-  //       width: 374,
-  //     },
-  //     itemId: '2d3b675ea857dc41158bad3b28300825',
-  //     url: '8bb438_7dde2abf38e24163b80c11619c357bf1.jpg',
-  //   },
-  //   {
-  //     metadata: {
-  //       height: 374,
-  //       width: 560,
-  //     },
-  //     itemId: '860df034014674abd7a2e73abe0b851c',
-  //     url: '8bb438_ac190df97cfe4c6d8bcd9ca8b6b3c100.jpg',
-  //   },
-  // ],
-  styles: {
-    galleryLayout: 0,
-    oneRow: false,
-    cubeRatio: 1,
-    galleryThumbnailsAlignment: 'bottom',
-    isVertical: false,
-    numberOfImagesPerRow: 3,
-    imageMargin: 5,
-    cubeType: 'fill',
-    enableInfiniteScroll: true,
-    titlePlacement: 'SHOW_ON_HOVER',
-    showArrows: false,
-    gridStyle: 1,
-    loveButton: false,
-    allowSocial: false,
-    allowDownload: false,
-    itemClick: 'nothing'
-  },
-  config: {
-    alignment: 'center',
-    size: 'content',
-    layout: 'small',
-    spacing: 0,
-  },
-};
 
 class GalleryComponent extends React.PureComponent {
   constructor(props) {
