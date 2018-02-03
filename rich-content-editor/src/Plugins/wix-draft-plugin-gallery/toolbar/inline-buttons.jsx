b7a103794df46b4bdfe189da9b6a2a0a5ebdb25e
lxgreen <alxgreen@gmail.com>
2018-02-04 01:41:45 +0200

    [rce] fix(gallery-inline-buttons): code clean up

--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/inline-buttons.jsx
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/toolbar/inline-buttons.jsx
@@ -1,98 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import AddIcon from '../icons/toolbar/icon-add.svg';
 import ManageMediaIcon from '../icons/insert-plugin.svg';
 import AdvancedSettingsIcon from '../../base/icons/block-settings.svg';
-import GridSmallIcon from '../icons/toolbar/layout/icon-grid-small.svg';
-import GridSmallIconActive from '../icons/toolbar/layout/icon-grid-small-active.svg';
-import GridMediumIcon from '../icons/toolbar/layout/icon-grid-medium.svg';
-import GridMediumIconActive from '../icons/toolbar/layout/icon-grid-medium-active.svg';
-import GridLargeIcon from '../icons/toolbar/layout/icon-grid-large.svg';
-import GridLargeIconActive from '../icons/toolbar/layout/icon-grid-large-active.svg';
 import GallerySettingsModal from '../components/gallery-settings-modal';
 import { BUTTONS } from '~/Plugins/base/buttons';
 import { getModalStyles } from '../../../Common/defaultModalStyles';
-
-class LayoutModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = this.stateFromProps(props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState(this.stateFromProps(nextProps));
-  }
-
-  stateFromProps = props => {
-    return {
-      layout: (props.componentData.config && props.componentData.config.layout) || 'small',
-    };
-  };
-
-  changeLayout = layout => {
-    const componentData = { ...this.props.componentData, config: { ...this.props.componentData.config, layout } };
-    this.props.store.set('componentData', componentData);
-  };
-
-  render = () => {
-    return (
-      <div>
-        <div onClick={() => this.changeLayout('small')}>{this.state.layout === 'small' ? <GridSmallIconActive /> : <GridSmallIcon />}</div>
-        <div onClick={() => this.changeLayout('medium')}>{this.state.layout === 'medium' ? <GridMediumIconActive /> : <GridMediumIcon />}</div>
-        <div onClick={() => this.changeLayout('large')}>{this.state.layout === 'large' ? <GridLargeIconActive /> : <GridLargeIcon />}</div>
-      </div>
-    );
-  };
-}
-
-LayoutModal.propTypes = {
-  store: PropTypes.object.isRequired,
-  helpers: PropTypes.object,
-  componentData: PropTypes.object.isRequired,
-  componentState: PropTypes.object.isRequired,
-};
-
-class SettingsModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = this.stateFromProps(props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState(this.stateFromProps(nextProps));
-  }
-
-  stateFromProps = props => {
-    return {
-      spacing: (props.componentData.config && props.componentData.config.spacing) || 0,
-    };
-  };
-
-  changeSpacing = event => {
-    const spacing = event.target.valueAsNumber;
-    const componentData = { ...this.props.componentData, config: { ...this.props.componentData.config, spacing } };
-    this.props.store.set('componentData', componentData);
-  };
-
-  render = () => {
-    return (
-      <div>
-        <label htmlFor="spacing">Spacing</label>
-        <input type="range" min="0" max="100" value={this.state.spacing} id="spacing" step="1" onChange={this.changeSpacing} />
-        <output htmlFor="spacing" id="spacingVal">
-          {this.state.spacing}px
-        </output>
-      </div>
-    );
-  };
-}
-
-SettingsModal.propTypes = {
-  store: PropTypes.object.isRequired,
-  helpers: PropTypes.object,
-  componentData: PropTypes.object.isRequired,
-  componentState: PropTypes.object.isRequired,
-};
 
 const modalStyles = getModalStyles();
 
