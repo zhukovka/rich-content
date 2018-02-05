06210aba1e5891f7499236a823b5ad00915d58aa
Guy Sopher <guyso@Super-Mac-2.local>
2018-02-05 18:01:36 +0200

    [rce] improve (gallery settings): more adjustments for mobile

--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/components/gallery-controls/gallery-settings-mobile-header.js
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/components/gallery-controls/gallery-settings-mobile-header.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classNames';
 import style from './gallery-settings-mobile-header.scss';
+import MoreIcon from '../../icons/more.svg';
 
 class GallerySettingsMobileHeader extends Component {
   render() {
     const { save, cancel } = this.props;
     return (
       <div>
-      <div className={style.headerPlaceholder} />
-      <div className={style.header}>
-        <a onClick={() => cancel()} className={classNames(style.button, style.cancel)}>{'Cancel'}</a>
-        <a onClick={() => save()} className={classNames(style.button, style.menu)}>{'.'}</a>
-        <a onClick={() => save()} className={classNames(style.button, style.done)}>{'Save'}</a>
-      </div>
+        <div className={style.headerPlaceholder} />
+        <div className={style.header}>
+          <a onClick={() => cancel()} className={classNames(style.button, style.cancel)}>{'Cancel'}</a>
+          <a onClick={() => {}} className={classNames(style.button, style.menu)}><MoreIcon/></a>
+          <a onClick={() => save()} className={classNames(style.button, style.done)}>{'Save'}</a>
+        </div>
       </div>
     );
   }
