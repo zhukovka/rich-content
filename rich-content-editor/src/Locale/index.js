baa14e6256a7f8f862e8e6b02d4fed35e66b324e
Jonathan Bachman <jonathanb@wix.com>
2018-03-11 17:40:48 +0200

    [rce] fix(Locale): try catch instead of checking for 'require.context'

--- rich-content-editor/src/Locale/index.js
+++ rich-content-editor/src/Locale/index.js
@@ -1,12 +1,12 @@
 const resources = {};
-if (require.context) {
+try {
   const requireContext = require.context('./', false, /\.json$/);
   requireContext.keys().forEach(key => {
     const translation = requireContext(key);
     const lang = key.substr(key.lastIndexOf('_') + 1, 2);
     resources[lang] = { translation };
   });
-} else {
+} catch (e) {
   resources.en = { translation: require('./messages_en.json') };
 }
 
