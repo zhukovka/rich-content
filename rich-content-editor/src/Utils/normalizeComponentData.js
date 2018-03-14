d9d52d0aeeb381713a21fdd2993097163a264821
Jonathan Bachman <jonathanb@wix.com>
2018-03-14 19:40:55 +0200

    [rce] fix(normalizeComponentData): adapted to blog ds

--- rich-content-editor/src/Utils/normalizeComponentData.js
+++ rich-content-editor/src/Utils/normalizeComponentData.js
@@ -1,16 +1,28 @@
 export default componentData => {
+  if (componentData.config) {
+    return null;
+  }
+
+  const config = {};
   const { alignment, size } = componentData;
   if (alignment || size) {
-    const config = {};
     if (alignment) {
       delete componentData.alignment; //eslint-disable-line fp/no-delete
       config.alignment = alignment;
+      config.size = 'small';
     }
     if (size) {
       delete componentData.size; //eslint-disable-line fp/no-delete
-      config.size = size;
+      if (size === 'smallCenter') {
+        config.size = 'small';
+      } else if (size === 'fullWidth') {
+        config.size = 'fullWidth';
+      }
+      config.alignment = 'center';
     }
-    return Object.assign({}, componentData, { config });
+  } else {
+    config.alignment = 'center';
+    config.size = 'content';
   }
-  return null;
+  return Object.assign({}, componentData, { config });
 };
