afb784f532b1061461bd0ac22556346ed3d58b47
lxgreen <alxgreen@gmail.com>
2018-05-02 13:51:40 +0300

    [rce] refactor(modals): code clean up

--- rich-content-editor/src/Plugins/wix-draft-plugin-gallery/components/gallery-settings-modal.js
+++ rich-content-editor/src/Plugins/wix-draft-plugin-gallery/components/gallery-settings-modal.js
@@ -14,9 +14,6 @@ import { SortableComponent } from './gallery-controls/gallery-items-sortable';
 import layoutData from '../helpers/layout-data-provider';
 import GallerySettingsMobileHeader from './gallery-controls/gallery-settings-mobile-header';
 
-function onFocus(e) {
-  console.log('focus e: ', e.target); // eslint-disable-line no-console
-}
 class ManageMediaSection extends Component {
 
   applyItems = items => {
@@ -140,7 +137,6 @@ export class GallerySettingsModal extends Component {
     this.setState({
       initComponentData: this.props.pubsub.get('componentData')
     });
-    document.body.addEventListener('focus', onFocus, true);
   }
 
   componentWillUnmount() {
