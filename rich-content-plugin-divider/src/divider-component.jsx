e7b71138287803de98ba3ee2f74b41c4d0cbf99a
lxgreen <alxgreen@gmail.com>
2018-05-14 17:43:57 +0300

    [rce] fix(a11y) divider plugin keys, semantics

--- rich-content-plugin-divider/src/divider-component.jsx
+++ rich-content-plugin-divider/src/divider-component.jsx
@@ -33,16 +33,24 @@ class DividerComponent extends PureComponent {
     };
   };
 
-  render() {
-    const { className, style } = this.props;
+  onKeyDown = (e, handler) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      handler();
+    }
+  };
 
+  render() {
+    const { className, style, onClick } = this.props;
+    /* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/no-noninteractive-element-interactions */
     return (
       <div
         style={{ width: this.state.width + '%', margin: 'auto', ...style }}
-        data-hook="divider" onClick={this.props.onClick}
-        className={classNames(className, this.styles[this.state.type])}
+        data-hook="divider" onClick={onClick} onKeyDown={e => this.onKeyDown(e, onClick)}
+        className={classNames(className, this.styles[this.state.type])} role="separator"
       />
     );
+    /* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/no-noninteractive-element-interactions */
+
   }
 }
 
