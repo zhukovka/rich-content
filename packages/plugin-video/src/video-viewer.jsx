1af661108150c1d4fb95c2055641560084ed420c
lxgreen <alxgreen@gmail.com>
2018-05-21 01:01:05 +0300

    [video-plugin]: viewer decoupling

---
+++ packages/plugin-video/src/video-viewer.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import { mergeStyles } from 'wix-rich-content-common';
+import styles from './default-video-styles.scss';
+
+class VideoViewer extends Component {
+  constructor(props) {
+    super(props);
+    this.styles = mergeStyles({ styles, theme: props.theme });
+  }
+
+  render() {
+    const { componentData, ...rest } = this.props;
+    return (
+      <ReactPlayer
+        className={classNames(this.styles.video_player)}
+        url={componentData.src}
+        {...rest}
+      />
+    );
+  }
+}
+
+VideoViewer.propTypes = {
+  componentData: PropTypes.object.isRequired,
+  theme: PropTypes.object,
+  onReady: PropTypes.func,
+  onStart: PropTypes.func,
+  controls: PropTypes.bool,
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+VideoViewer.defaultProps = {
+  width: '100%',
+  height: '100%',
+  controls: true,
+};
+
+export default VideoViewer;
