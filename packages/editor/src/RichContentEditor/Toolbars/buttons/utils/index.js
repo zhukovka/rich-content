c1e4388517a410129aeb24d03e813db6b2af5505
Jonathan Bachman <bchmn@users.noreply.github.com>
2018-05-30 14:07:27 +0300

    feat(editor): createTextDropdownButton + text alignment dropdown button (#36)

--- packages/editor/src/RichContentEditor/Toolbars/buttons/utils/index.js
+++ packages/editor/src/RichContentEditor/Toolbars/buttons/utils/index.js
@@ -1,5 +1,6 @@
 export { default as createTextAlignmentButton } from './createTextAlignmentButton';
 export { default as createTextBlockStyleButton } from './createTextBlockStyleButton';
 export { default as createTextInlineStyleButton } from './createTextInlineStyleButton';
+export { default as createTextDropdownButton } from './createTextDropdownButton';
 export { default as createThemedSeparator } from './createThemedSeparator';
 export { default as getTextButtonsFromList } from './getTextButtonsFromList';
