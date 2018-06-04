28d7c21a59c37f4b41c3002872166c14f2a5f240
lxgreen <alxgreen@gmail.com>
2018-06-04 13:37:48 +0300

    [common] fix(a11y): outline is visible on tab navigation, not on click

---
+++ packages/common/src/Components/AccessibilityListener.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import styles from '../Styles/global.scss';
+
+export default class AccessibilityListener extends Component {
+
+  handleTabKeyUp = e => {
+    if (e.which === 9 && document.body.classList.contains(styles.noOutline)) {
+      document.body.classList.remove(styles.noOutline);
+    }
+  };
+
+  handleClick = () => {
+    if (!document.body.classList.contains(styles.noOutline)) {
+      document.body.classList.add(styles.noOutline);
+    }
+  };
+
+  componentDidMount() {
+    document.body.classList.add(styles.noOutline);
+
+    if (!this.props.isMobile) {
+      document.addEventListener('keyup', this.handleTabKeyUp);
+      document.addEventListener('click', this.handleClick);
+    }
+  }
+
+  componentWillUnmount() {
+    if (!this.props.isMobile) {
+      document.removeEventListener('keyup', this.handleTabKeyUp);
+      document.removeEventListener('click', this.handleClick);
+    }
+  }
+
+  render = () => null;
+}
+
+AccessibilityListener.propTypes = {
+  isMobile: PropTypes.bool,
+};
--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -31,6 +31,7 @@ export { default as createBaseToolbar } from './Base/createBaseToolbar';
 export { default as baseToolbarButton } from './Base/baseToolbarButton';
 
 // Components
+export { default as AccessibilityListener } from './Components/AccessibilityListener';
 export { default as Button } from './Components/Button';
 export { default as Checkbox } from './Components/Checkbox';
 export { default as Dropdown } from './Components/Dropdown';
