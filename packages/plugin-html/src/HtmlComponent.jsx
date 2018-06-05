bb4ab3d05c0f34c172a4159c9b0f544bfcdaa77c
lxgreen <alxgreen@gmail.com>
2018-06-05 18:26:16 +0300

    [html] feat(schema-validation): html component data schema added and validated

--- packages/plugin-html/src/HtmlComponent.jsx
+++ packages/plugin-html/src/HtmlComponent.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { mergeStyles, isValidUrl, normalizeURL } from 'wix-rich-content-common';
+import { mergeStyles, isValidUrl, normalizeURL, ComponentDataValidator } from 'wix-rich-content-common';
 
 import { SRC_TYPE_HTML, SRC_TYPE_URL, DEFAULT_COMPONENT_DATA } from './constants';
+import schema from './data-schema.json';
 import IframeHtml from './IframeHtml';
 import IframeUrl from './IframeUrl';
 import htmlComponentStyles from './HtmlComponent.scss';
 
+const dataValidator = new ComponentDataValidator();
+
 const HtmlComponent = props => {
   const styles = mergeStyles({ styles: htmlComponentStyles, theme: props.theme });
+  dataValidator.validate(schema, props.componentData);
   const { blockProps, componentData: { src, srcType } } = props;
   const readOnly = blockProps ? blockProps.readOnly : true;
 
