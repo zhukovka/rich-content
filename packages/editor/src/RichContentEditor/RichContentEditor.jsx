e534d1fb509f265cf84e0ea4f897ffeb8c1618a9
Jonathan Bachman <jonathanb@wix.com>
2018-06-06 11:54:05 +0300

    [editor] refactor(blockStyleFn): extracted to separate file

--- packages/editor/src/RichContentEditor/RichContentEditor.jsx
+++ packages/editor/src/RichContentEditor/RichContentEditor.jsx
@@ -12,6 +12,7 @@ import createEditorToolbars from './Toolbars';
 import createPlugins from './createPlugins';
 import { keyBindingFn } from './keyBindings';
 import handleKeyCommand from './handleKeyCommand';
+import blockStyleFn from './blockStyleFn';
 import { EditorModals, AccessibilityListener, getModalStyles } from 'wix-rich-content-common';
 import normalizeInitialState from './normalizeInitialState';
 import styles from '~/Styles/rich-content-editor.scss';
@@ -143,59 +144,6 @@ class RichContentEditor extends Component {
     return element && element.querySelector('*[tabindex="0"]');
   }
 
-  blockStyleFn = contentBlock => {
-    const { type, data: { textAlignment } } = contentBlock.toJS();
-    const classList = [];
-    const { theme } = this.state;
-
-    switch (type) {
-      case 'blockquote':
-        classList.push(styles.quote);
-        classList.push(theme.quote);
-        break;
-      case 'header-one':
-        classList.push(styles.headerOne);
-        classList.push(theme.headerOne);
-        break;
-      case 'header-two':
-        classList.push(styles.headerTwo);
-        classList.push(theme.headerTwo);
-        break;
-      case 'header-three':
-        classList.push(styles.headerThree);
-        classList.push(theme.headerThree);
-        break;
-      case 'indent':
-        classList.push(styles.indent);
-        classList.push(theme.indent);
-        break;
-      case 'ordered-list-item':
-        classList.push(styles.orderedList);
-        classList.push(theme.orderedList);
-        break;
-      case 'unordered-list-item':
-        classList.push(styles.unorderedList);
-        classList.push(theme.unorderedList);
-        break;
-      case 'atomic':
-        classList.push(styles.atomic);
-        classList.push(theme.atomic);
-        break;
-      case 'code-block':
-        classList.push(styles.codeBlock);
-        classList.push(theme.codeBlock);
-        break;
-      default:
-        classList.push(styles.text);
-        classList.push(theme.text);
-    }
-    if (type !== 'atomic') {
-      classList.push(styles[textAlignment]);
-      classList.push(theme[textAlignment]);
-    }
-    return classNames(...classList);
-  };
-
   getEditorState = () => this.state.editorState;
 
   updateEditorState = editorState => {
@@ -265,14 +213,14 @@ class RichContentEditor extends Component {
       onBlur,
       onFocus,
     } = this.props;
-    const { editorState, readOnly } = this.state;
+    const { editorState, readOnly, theme } = this.state;
     return (
       <Editor
         ref={this.setEditor}
         editorState={editorState}
         onChange={this.updateEditorState}
         plugins={this.plugins}
-        blockStyleFn={this.blockStyleFn}
+        blockStyleFn={blockStyleFn(theme)}
         handleKeyCommand={handleKeyCommand(this.updateEditorState)}
         editorKey={editorKey}
         keyBindingFn={keyBindingFn}
