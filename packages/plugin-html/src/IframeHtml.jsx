0db7bacd6848f0507469d92805be851beefe60b3
Artur Charcenko <artur.charcenko@gmail.com>
2018-06-07 16:28:58 +0300

    [html] fix(Html) Rerender html iframe (#42)

--- packages/plugin-html/src/IframeHtml.jsx
+++ packages/plugin-html/src/IframeHtml.jsx
@@ -1,59 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import omit from 'lodash/omit';
 import Iframe from './Iframe';
 
-class IframeHtml extends Component {
-  componentWillReceiveProps(nextProps) {
-    if (this.props.html !== nextProps.html) {
-      this.updateIframeContent(nextProps.html);
-    }
-  }
+const getIframeContent = html => `
+  <html>
+    <head></head>
+    <body>${html}</body>
+  </html>
+`;
 
-  updateIframeContent = src => {
-    this.iframe && this.iframe.contentWindow.postMessage(src, '*');
-  };
-
-  handleIframeLoad = () => {
-    this.updateIframeContent(this.props.html);
-  };
-
-  setIframe = iframe => {
-    this.iframe = iframe;
-  };
-
-  getIframeContent = () => {
-    return `
-      <html>
-        <head>
-          <script>
-            window.addEventListener(
-              'message', 
-              function(event) {
-                if (event.origin.indexOf('${window.origin}') >= 0) {
-                  document.body.innerHTML = event.data;
-                }
-              }
-            );
-          </script>
-        </head>
-        <body></body>
-      </html>
-    `;
-  };
-
-  render() {
-    return (
-      <Iframe
-        {...omit(this.props, 'html')}
-        iframeRef={this.setIframe}
-        onLoad={this.handleIframeLoad}
-        srcDoc={this.getIframeContent()}
-        sandbox="allow-presentation allow-forms allow-scripts"
-      />
-    );
-  }
-}
+const IframeHtml = ({ html, ...otherProps }) => (
+  <Iframe
+    {...otherProps}
+    srcDoc={getIframeContent(html)}
+    sandbox="allow-presentation allow-forms allow-scripts"
+  />
+);
 
 IframeHtml.propTypes = {
   html: PropTypes.string.isRequired,
