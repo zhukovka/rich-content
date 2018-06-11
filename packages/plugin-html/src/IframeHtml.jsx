c926f2f42752bc142bce13d58b5073e259e49fc6
artur-charcenko <arturc@wix.com>
2018-06-11 15:38:14 +0300

    [html] feat(IframeHtml) Serve code from external HTML file

--- packages/plugin-html/src/IframeHtml.jsx
+++ packages/plugin-html/src/IframeHtml.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import omit from 'lodash/omit';
 import Iframe from './Iframe';
 
-const getIframeContent = html => `
-  <html>
-    <head></head>
-    <body>${html}</body>
-  </html>
-`;
+class IframeHtml extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (this.props.html !== nextProps.html) {
+      this.updateIframeContent(nextProps.html);
+    }
+  }
 
-const IframeHtml = ({ html, ...otherProps }) => (
-  <Iframe
-    {...otherProps}
-    srcDoc={getIframeContent(html)}
-    sandbox="allow-presentation allow-forms allow-scripts"
-  />
-);
+  updateIframeContent = content => {
+    this.shouldIgnoreLoad = true;
+    this.iframe.contentWindow.postMessage({
+      type: 'htmlPlugin:updateContent',
+      content,
+    }, '*');
+  };
+
+  handleIframeLoad = () => {
+    !this.shouldIgnoreLoad && this.updateIframeContent(this.props.html);
+  };
+
+  setIframe = iframe => {
+    this.iframe = iframe;
+  };
+
+  render() {
+    return (
+      <Iframe
+        {...omit(this.props, 'html')}
+        iframeRef={this.setIframe}
+        onLoad={this.handleIframeLoad}
+        src="http://127.0.0.1:8080/html-plugin.html"
+      />
+    );
+  }
+}
 
 IframeHtml.propTypes = {
   html: PropTypes.string.isRequired,
