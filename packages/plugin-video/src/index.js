eeb07ed0917f555c44ffadb7ec17d2574fb05b00
Jonathan Bachman <jonathanb@wix.com>
2018-06-14 16:00:53 +0300

    [video] fix(index): use default export from 'video-viewer'

--- packages/plugin-video/src/index.js
+++ packages/plugin-video/src/index.js
@@ -1,5 +1,5 @@
 export { createVideoPlugin } from './createVideoPlugin';
-export { VideoViewer } from './video-viewer';
+export { default as VideoViewer } from './video-viewer';
 export { VIDEO_TYPE_LEGACY, VIDEO_TYPE } from './types';
 export { Modals, ModalsMap } from './modals';
 export { typeMapper as videoTypeMapper } from './typeMapper';
