03abb6df01f4c3e34c938d6f2fab3063ec41afe6
lxgreen <alxgreen@gmail.com>
2018-06-24 17:25:00 +0300

    fix(handleKeyCommand): accepts custom handlers map to support context-dependent command cases

--- packages/editor/src/RichContentEditor/handleKeyCommand.js
+++ packages/editor/src/RichContentEditor/handleKeyCommand.js
@@ -1,59 +1,43 @@
 import { EditorState, RichUtils, Modifier } from '@wix/draft-js';
 import {
-  hasLinksInSelection,
-  removeLinksInSelection,
   isAtomicBlockFocused,
   removeBlock,
 } from 'wix-rich-content-common';
-import { getStaticTextToolbarId } from './Toolbars/toolbar-id';
 import { COMMANDS } from './keyBindings';
 
 /* eslint-disable complexity */
-export default updateEditorState => (
+export default (updateEditorState, customHandlers) => (
   (command, editorState) => {
     let newState, contentState;
-    switch (command) {
-      case COMMANDS.LINK:
-        if (hasLinksInSelection(editorState)) {
-          newState = removeLinksInSelection(editorState);
-        } else {
-          this.openLinkModal();
-          return 'handled';
-        }
-        break;
-      case COMMANDS.ALIGN_RIGHT:
-      case COMMANDS.ALIGN_LEFT:
-      case COMMANDS.ALIGN_CENTER:
-      case COMMANDS.JUSTIFY:
-        contentState = Modifier.mergeBlockData(editorState.getCurrentContent(), editorState.getSelection(), { textAlignment: command });
-        newState = EditorState.push(editorState, contentState, 'change-block-data');
-        break;
-      case COMMANDS.TITLE:
-      case COMMANDS.SUBTITLE:
-      case COMMANDS.NUMBERED_LIST:
-      case COMMANDS.BULLET_LIST:
-      case COMMANDS.BLOCKQUOTE:
-      case COMMANDS.CODE:
-        newState = RichUtils.toggleBlockType(editorState, command);
-        break;
-      case COMMANDS.TAB:
-        if (this.getToolbars().TextToolbar) {
-          const staticToolbarButton = this.findFocusableChildForElement(`${getStaticTextToolbarId(this.refId)}`);
-          staticToolbarButton && staticToolbarButton.focus();
-          return 'handled';
-        } else {
-          this.editor.blur();
-          return 'not-handled';
-        }
-      case COMMANDS.BACKSPACE:
-      case COMMANDS.DELETE:
-        if (isAtomicBlockFocused(editorState)) {
-          newState = removeBlock(editorState, editorState.getSelection().getAnchorKey());
-        }
-        break;
-      default:
-        newState = RichUtils.handleKeyCommand(editorState, command);
-        break;
+    if (customHandlers[command]) {
+      newState = customHandlers[command](editorState);
+    } else {
+      switch (command) {
+        case COMMANDS.ALIGN_RIGHT:
+        case COMMANDS.ALIGN_LEFT:
+        case COMMANDS.ALIGN_CENTER:
+        case COMMANDS.JUSTIFY:
+          contentState = Modifier.mergeBlockData(editorState.getCurrentContent(), editorState.getSelection(), { textAlignment: command });
+          newState = EditorState.push(editorState, contentState, 'change-block-data');
+          break;
+        case COMMANDS.TITLE:
+        case COMMANDS.SUBTITLE:
+        case COMMANDS.NUMBERED_LIST:
+        case COMMANDS.BULLET_LIST:
+        case COMMANDS.BLOCKQUOTE:
+        case COMMANDS.CODE:
+          newState = RichUtils.toggleBlockType(editorState, command);
+          break;
+        case COMMANDS.BACKSPACE:
+        case COMMANDS.DELETE:
+          if (isAtomicBlockFocused(editorState)) {
+            newState = removeBlock(editorState, editorState.getSelection().getAnchorKey());
+          }
+          break;
+        default:
+          newState = RichUtils.handleKeyCommand(editorState, command);
+          break;
+      }
     }
 
     if (newState) {
