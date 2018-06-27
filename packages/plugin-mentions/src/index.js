71274dc6ee87fe7dc33601151b9db1f1c0d0abc9
Vytenis Butkevicius <vytenisb@wix.com>
2018-06-27 14:49:41 +0300

    add navigation helper for tpa

--- packages/plugin-mentions/src/index.js
+++ packages/plugin-mentions/src/index.js
@@ -1,2 +1,3 @@
 export { createMentionsPlugin } from './createMentionsPlugin';
 export { EXTERNAL_MENTIONS_TYPE } from './types';
+export { createMembersAreaNavigationHandler } from './tpa-utils/membersAreaNavigationHandler';
