6984484c06f316da6a36c75a30d81fe43630e6fb
lxgreen <alxgreen@gmail.com>
2018-06-28 11:28:09 +0300

    fix(data-validation): temporary cancelled

--- packages/common/src/Utils/component-data-validator.js
+++ packages/common/src/Utils/component-data-validator.js
@@ -1,10 +1,1 @@
-import { Validator } from 'jsonschema';
-
-const validator = new Validator();
-
-export const validate = (componentData, schema) => {
-  const result = validator.validate(componentData, schema);
-  if (!result.valid) {
-    throw result.errors[0];
-  }
-};
+export const validate = () => true;
