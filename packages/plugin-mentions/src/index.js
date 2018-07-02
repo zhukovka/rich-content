97161462652ef165dbfb2e5f2e5d718446a6c807
Vytenis Butkevicius <vytenisb@wix.com>
2018-07-02 10:30:03 +0300

    [mentions-plugin] remove tpa utils

--- packages/plugin-mentions/src/index.js
+++ packages/plugin-mentions/src/index.js
@@ -1,3 +1,2 @@
 export { createExternalMentionsPlugin } from './createMentionsPlugin';
 export { EXTERNAL_MENTIONS_TYPE } from './types';
-export { createMembersAreaNavigationHandler } from './tpa-utils/membersAreaNavigationHandler';
