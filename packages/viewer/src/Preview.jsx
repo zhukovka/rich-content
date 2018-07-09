1d7ad00ebdbf86169dca328920ddd8f3bb1b749a
lxgreen <alxgreen@gmail.com>
2018-07-10 00:32:21 +0300

    [viewer] feat(inline-styles): strategy injected; redraft renderers styled

--- packages/viewer/src/Preview.jsx
+++ packages/viewer/src/Preview.jsx
@@ -1,64 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import redraft from 'redraft';
+import { createInlineStyleDecorators, mergeStyles } from 'wix-rich-content-common';
 import getPluginsViewer from './PluginsViewer';
 import List from './List';
-import { NewLineStrategy, NewLineDecoration } from './decorators/soft-new-line-decorator';
+import { getStrategyByStyle } from './decorators/getStrategyByStyle';
+import styles from '../statics/rich-content-viewer.scss';
 
-// import './Preview.css';
+const addBlock = children => <div>{children}</div>;
 
-const styles = {
-  code: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
-  codeBlock: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 20,
-  },
+const getInline = mergedStyles => {
+  return {
+    BOLD: (children, { key }) => <strong key={key}>{children}</strong>,
+    ITALIC: (children, { key }) => <em key={key}>{children}</em>,
+    UNDERLINE: (children, { key }) => <u key={key}>{children}</u>,
+    CODE: (children, { key }) => <span key={key} className={mergedStyles.code}>{children}</span>
+  };
 };
 
-const inline = {
-  BOLD: (children, { key }) => <strong key={key}>{children}</strong>,
-  ITALIC: (children, { key }) => <em key={key}>{children}</em>,
-  UNDERLINE: (children, { key }) => <u key={key}>{children}</u>,
-  CODE: (children, { key }) => <span key={key} style={styles.code}>{children}</span>,
-};
-
-
-/* eslint-disable react/jsx-key */
-const addBreaklines = children => children.map(child => [child, <br />]);
-
-const getList = ordered =>
-  (children, { depth, keys }) => (
-    <List key={keys[0]} keys={keys} depth={depth} ordered={ordered}>
-      {children.map((child, i) => <li key={keys[i]} >{child}</li>)}
-    </List>
-  );
-
+const getList = (ordered, mergedStyles) =>
+  (children, { depth, keys }) => {
+    const className = ordered ? 'ordered' : 'unordered';
+    return (
+      <List key={keys[0]} keys={keys} depth={depth} ordered={ordered}>
+        {children.map((child, i) => <li className={mergedStyles[`${className}List`]} key={keys[i]} >{addBlock(child, mergedStyles)}</li>)}
+      </List>
+    );
+  };
 
 /**
  * Note that children can be maped to render a list or do other cool stuff
  */
-const blocks = {
+const getBlocks = mergedStyles => {
   // Rendering blocks like this along with cleanup results in a single p tag for each paragraph
   // adding an empty block closes current paragraph and starts a new one
-  unstyled: (children, { keys }) => <p key={keys[0]}>{addBreaklines(children)}</p>,
-  blockquote:
-    (children, { keys }) => <blockquote key={keys[0]} >{addBreaklines(children)}</blockquote>,
-  'header-one': (children, { keys }) => children.map((child, i) => <h1 key={keys[i]}>{child}</h1>),
-  'header-two': (children, { keys }) => children.map((child, i) => <h2 key={keys[i]}>{child}</h2>),
-  'header-three': (children, { keys }) => children.map((child, i) => <h3 key={keys[i]}>{child}</h3>),
-  'header-four': (children, { keys }) => children.map((child, i) => <h4 key={keys[i]}>{child}</h4>),
-  'header-five': (children, { keys }) => children.map((child, i) => <h5 key={keys[i]}>{child}</h5>),
-  'header-six': (children, { keys }) => children.map((child, i) => <h6 key={keys[i]}>{child}</h6>),
-  'code-block': (children, { keys }) => <pre key={keys[0]} style={styles.codeBlock}>{addBreaklines(children)}</pre>,
-  'unordered-list-item': getList(),
-  'ordered-list-item': getList(true),
+  return {
+    unstyled: (children, { keys }) => <p key={keys[0]}>{children}</p>,
+    blockquote: (children, { keys }) => <blockquote className={mergedStyles.quote} key={keys[0]} >{children}</blockquote>,
+    'header-one': (children, { keys }) => children.map((child, i) => <h2 className={mergedStyles.headerOne} key={keys[i]}>{child}</h2>),
+    'header-two': (children, { keys }) => children.map((child, i) => <h3 className={mergedStyles.headerTwo} key={keys[i]}>{child}</h3>),
+    'header-three': (children, { keys }) => children.map((child, i) => <h4 className={mergedStyles.headerThree} key={keys[i]}>{child}</h4>),
+    'header-four': (children, { keys }) => children.map((child, i) => <h5 key={keys[i]}>{child}</h5>),
+    'header-five': (children, { keys }) => children.map((child, i) => <h6 key={keys[i]}>{child}</h6>),
+    'code-block': (children, { keys }) => <pre key={keys[0]} className={mergedStyles.codeBlock}>{children}</pre>,
+    'unordered-list-item': getList(false, mergedStyles),
+    'ordered-list-item': getList(true, mergedStyles),
+  };
 };
 
 const getEntities = (typeMap, pluginProps) => ({
@@ -69,7 +56,6 @@ const combineTypeMappers = mappers => {
   if (!mappers || !mappers.length || mappers.some(resolver => typeof resolver !== 'function')) {
     throw new TypeError('typeMappers is expected to be a function array');
   }
-
   return mappers.reduce((map, mapper) => Object.assign(map, mapper()), {});
 };
 
@@ -84,22 +70,23 @@ const options = {
 };
 
 const Preview = ({ raw, typeMappers, theme, isMobile, decorators, anchorTarget, relValue }) => {
+  const mergedStyles = mergeStyles({ styles, theme });
   const isEmpty = isEmptyRaw(raw);
   const typeMap = combineTypeMappers(typeMappers);
-  const combineDecorators = [...decorators, {
-    strategy: NewLineStrategy,
-    component: NewLineDecoration
-  }];
+  const combinedDecorators = [
+    ...decorators,
+    ...createInlineStyleDecorators(getStrategyByStyle, mergedStyles)
+  ];
   window.redraft = redraft;
   return (
-    <div className="Preview">
-      {isEmpty && <div className="Preview-empty">There is nothing to render...</div>}
+    <div className={mergedStyles.preview}>
+      {isEmpty && <div>There is nothing to render...</div>}
       {!isEmpty &&
         redraft(raw, {
-          inline,
-          blocks,
+          inline: getInline(mergedStyles),
+          blocks: getBlocks(mergedStyles),
           entities: getEntities(typeMap, { theme, isMobile, anchorTarget, relValue }),
-          decorators: combineDecorators },
+          decorators: combinedDecorators },
         options)}
     </div>
   );
