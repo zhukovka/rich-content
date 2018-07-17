3f36c2f8935fbcf6b5f63dedc5b6a6249acf747c
lxgreen <alxgreen@gmail.com>
2018-07-17 14:51:09 +0300

    [image] refactor(image-viewer): imageProps initialization readable

--- packages/plugin-image/src/image-viewer.jsx
+++ packages/plugin-image/src/image-viewer.jsx
@@ -139,9 +139,14 @@ class ImageViewer extends React.Component {
     const itemClassName = classNames(styles.imageContainer, className);
     const imageClassName = classNames(styles.image);
     const imageSrc = this.getImageSrc(data.src);
-    const imageProps = data.src && settings && isFunction(settings.imageProps) ? settings.imageProps(data.src) : settings && settings.imageProps;
+    let imageProps = {};
+    if (data.src && settings && isFunction(settings.imageProps)) {
+      imageProps = settings.imageProps(data.src);
+    } else if (data.src && settings) {
+      imageProps = settings.imageProps;
+    }
 
-    /* eslint-disable jsx-a11y/no-static-element-interactions */
+
     return (
       <div
         data-hook="imageViewer" onClick={onClick} className={itemClassName} onKeyDown={e => this.onKeyDown(e, onClick)}
@@ -156,7 +161,7 @@ class ImageViewer extends React.Component {
         {shouldRenderCaption && this.renderCaption(metadata.caption, isFocused, readOnly, styles, defaultCaption)}
       </div>
     );
-    /* eslint-enable jsx-a11y/no-static-element-interactions */
+
   }
 }
 
