49e45e3e6dc8d70af223b735912c95bfc12bccd2
lxgreen <alxgreen@gmail.com>
2018-07-23 00:15:05 +0300

    [common] refactor(COMMANDS): key commands and modifiers moved to common

---
+++ packages/common/src/consts.js
@@ -0,0 +1,21 @@
+export const COMMANDS = {
+  TITLE: 'header-two',
+  SUBTITLE: 'header-three',
+  ALIGN_LEFT: 'left',
+  ALIGN_RIGHT: 'right',
+  ALIGN_CENTER: 'center',
+  JUSTIFY: 'justify',
+  NUMBERED_LIST: 'ordered-list-item',
+  BULLET_LIST: 'unordered-list-item',
+  CODE: 'code-block',
+  BLOCKQUOTE: 'blockquote',
+  BACKSPACE: 'backspace',
+  DELETE: 'delete',
+};
+
+export const MODIFIERS = {
+  COMMAND: 'command',
+  CTRL: 'ctrl',
+  OPTION: 'option',
+  SHIFT: 'shift',
+};
