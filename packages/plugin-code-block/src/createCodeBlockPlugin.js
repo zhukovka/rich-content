dfb51cb0b116ca07d7b9fbd4c26343dbdcdcd43a
Vytenis Butkevicius <vytenis.butkevicius@gmail.com>
2018-08-02 13:45:33 +0300

    [code-block] change default position in toolbar, allow to configure it (#72)
    * [code-block] change default code-block position in mobile toolbar, allow to change it with settings
    
    * [code-block] allow to set whole position object

--- packages/plugin-code-block/src/createCodeBlockPlugin.js
+++ packages/plugin-code-block/src/createCodeBlockPlugin.js
@@ -1,8 +1,15 @@
 import CodeUtils from 'draft-js-code';
+import merge from 'lodash/merge';
 import { createBasePlugin } from 'wix-rich-content-common';
 import { CODE_BLOCK_TYPE } from './types';
 import PrismDecorator from './PrismDecorator';
 import createCodeBlockToolbar from './toolbar/createCodeBlockToolbar';
+
+const defaultSettings = {
+  position: {
+    mobile: 7,
+  },
+};
 
 const createUnderlyingPlugin = ({ theme }) => ({
   keyBindingFn: (event, { getEditorState }) => {
@@ -55,7 +62,19 @@ const createUnderlyingPlugin = ({ theme }) => ({
 
 const createCodeBlockPlugin = (config = {}) => {
   const type = CODE_BLOCK_TYPE;
-  const { decorator, helpers, theme, isMobile, t, anchorTarget, relValue, getEditorState, setEditorState } = config;
+  const {
+    decorator,
+    helpers,
+    theme,
+    isMobile,
+    t,
+    anchorTarget,
+    relValue,
+    getEditorState,
+    setEditorState,
+    codeBlock: userSettings = {},
+  } = config;
+  const settings = merge(defaultSettings, userSettings);
   const plugin = createUnderlyingPlugin({ theme });
   const toolbar = createCodeBlockToolbar({
     helpers,
@@ -65,20 +84,24 @@ const createCodeBlockPlugin = (config = {}) => {
     anchorTarget,
     relValue,
     getEditorState,
-    setEditorState
+    setEditorState,
+    settings,
   });
 
-  return createBasePlugin({
-    decorator,
-    theme,
-    toolbar,
-    type,
-    helpers,
-    isMobile,
-    anchorTarget,
-    relValue,
-    t
-  }, plugin);
+  return createBasePlugin(
+    {
+      decorator,
+      theme,
+      toolbar,
+      type,
+      helpers,
+      isMobile,
+      anchorTarget,
+      relValue,
+      t,
+    },
+    plugin,
+  );
 };
 
 export { createCodeBlockPlugin };
