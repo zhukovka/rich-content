1e499da50449823837a51c29d354b29471a6781e
Jonathan Bachman <jonathanb@wix.com>
2019-01-09 17:34:22 +0200

    [mentions] chore(linter): MentionViewer

--- packages/plugin-mentions/src/MentionViewer.jsx
+++ packages/plugin-mentions/src/MentionViewer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { mergeStyles } from 'wix-rich-content-common';
 import MentionComponent from './MentionComponent';
 import Styles from '../statics/mentions.scss';
@@ -6,6 +7,11 @@ import Styles from '../statics/mentions.scss';
 const MentionViewer = props => {
   const theme = mergeStyles({ styles: Styles, theme: props.theme });
   return <MentionComponent mention={props.componentData.mention} {...props} theme={theme} />;
+};
+
+MentionViewer.propTypes = {
+  theme: PropTypes.object,
+  componentData: PropTypes.object.isRequired,
 };
 
 export default MentionViewer;
