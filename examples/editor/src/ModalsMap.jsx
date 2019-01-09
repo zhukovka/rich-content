eb332d677f81dfda759a85ef40461d20c62a3ca4
Jonathan Bachman <jonathanb@wix.com>
2019-01-09 11:34:44 +0200

    [editor example] enabled emoji, image and gallery plugins

--- examples/editor/src/ModalsMap.jsx
+++ examples/editor/src/ModalsMap.jsx
@@ -1,10 +1,14 @@
 import { ModalsMap as VideoModalsMap } from 'wix-rich-content-plugin-video';
+import { ModalsMap as ImageModalsMap } from 'wix-rich-content-plugin-image';
+import { ModalsMap as GalleryModalsMap } from 'wix-rich-content-plugin-gallery';
 import { ModalsMap as SoundCloudModalsMap } from 'wix-rich-content-plugin-sound-cloud';
 import { ModalsMap as GiphyModalsMap } from 'wix-rich-content-plugin-giphy';
 import { ModalsMap as ImageModalsMap } from 'wix-rich-content-plugin-image';
 
 export default {
   ...VideoModalsMap,
+  ...ImageModalsMap,
+  ...GalleryModalsMap,
   ...SoundCloudModalsMap,
   ...GiphyModalsMap,
   ...ImageModalsMap,
--- examples/editor/src/Plugins.jsx
+++ examples/editor/src/Plugins.jsx
@@ -1,8 +1,9 @@
 import { createLinkPlugin } from 'wix-rich-content-plugin-link';
 import { createHashtagPlugin } from 'wix-rich-content-plugin-hashtag';
-// import { createExternalEmojiPlugin } from 'wix-rich-content-plugin-emoji';
+import { createExternalEmojiPlugin } from 'wix-rich-content-plugin-emoji';
 import { createImagePlugin } from 'wix-rich-content-plugin-image';
 import { createVideoPlugin } from 'wix-rich-content-plugin-video';
+import { createGalleryPlugin } from 'wix-rich-content-plugin-gallery';
 import { createHtmlPlugin } from 'wix-rich-content-plugin-html';
 import { createDividerPlugin } from 'wix-rich-content-plugin-divider';
 import { createExternalMentionsPlugin } from 'wix-rich-content-plugin-mentions';
@@ -15,22 +16,26 @@ import 'wix-rich-content-common/dist/styles.min.css';
 import 'wix-rich-content-editor/dist/styles.min.css';
 // import 'wix-rich-content-plugin-code-block/dist/styles.min.css';
 import 'wix-rich-content-plugin-divider/dist/styles.min.css';
-// import 'wix-rich-content-plugin-emoji/dist/styles.min.css';
+import 'wix-rich-content-plugin-emoji/dist/styles.min.css';
 import 'wix-rich-content-plugin-html/dist/styles.min.css';
 import 'wix-rich-content-plugin-hashtag/dist/styles.min.css';
 import 'wix-rich-content-plugin-link/dist/styles.min.css';
 import 'wix-rich-content-plugin-mentions/dist/styles.min.css';
 import 'wix-rich-content-plugin-image/dist/styles.min.css';
 import 'wix-rich-content-plugin-video/dist/styles.min.css';
+import 'wix-rich-content-plugin-image/dist/styles.min.css';
+import 'wix-rich-content-plugin-gallery/dist/styles.min.css';
 import 'wix-rich-content-plugin-sound-cloud/dist/styles.min.css';
 import 'wix-rich-content-plugin-giphy/dist/styles.min.css';
 
 const plugins = [
   createImagePlugin,
   createVideoPlugin,
+  createImagePlugin,
+  createGalleryPlugin,
   createHtmlPlugin,
   createDividerPlugin,
-  // createExternalEmojiPlugin,
+  createExternalEmojiPlugin,
   createLinkPlugin,
   createHashtagPlugin,
   createExternalMentionsPlugin,
