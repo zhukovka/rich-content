f2b4255621d820586a037eb0c1e6db972cedf108
Jonathan Bachman <jonathanb@wix.com>
2019-01-16 15:11:19 +0200

    [editor example] fix(ModalsMap): added image modals map

--- examples/editor/src/ModalsMap.jsx
+++ examples/editor/src/ModalsMap.jsx
@@ -1,9 +1,11 @@
 import { ModalsMap as VideoModalsMap } from 'wix-rich-content-plugin-video';
 import { ModalsMap as SoundCloudModalsMap } from 'wix-rich-content-plugin-sound-cloud';
 import { ModalsMap as GiphyModalsMap } from 'wix-rich-content-plugin-giphy';
+import { ModalsMap as ImageModalsMap } from 'wix-rich-content-plugin-image';
 
 export default {
   ...VideoModalsMap,
   ...SoundCloudModalsMap,
   ...GiphyModalsMap,
+  ...ImageModalsMap,
 };
