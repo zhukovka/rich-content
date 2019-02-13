19f76c85d17eea75c53672136f88e7e4c995b9ea
shaulg <shaulg@wix.com>
2019-02-13 11:27:49 +0200

    [examples] fix(ModalsMap) duplicate ImageModalsMap

--- examples/editor/src/ModalsMap.jsx
+++ examples/editor/src/ModalsMap.jsx
@@ -1,5 +1,4 @@
 import { ModalsMap as VideoModalsMap } from 'wix-rich-content-plugin-video';
-import { ModalsMap as ImageModalsMap } from 'wix-rich-content-plugin-image';
 import { ModalsMap as GalleryModalsMap } from 'wix-rich-content-plugin-gallery';
 import { ModalsMap as SoundCloudModalsMap } from 'wix-rich-content-plugin-sound-cloud';
 import { ModalsMap as GiphyModalsMap } from 'wix-rich-content-plugin-giphy';
@@ -7,7 +6,6 @@ import { ModalsMap as ImageModalsMap } from 'wix-rich-content-plugin-image';
 
 export default {
   ...VideoModalsMap,
-  ...ImageModalsMap,
   ...GalleryModalsMap,
   ...SoundCloudModalsMap,
   ...GiphyModalsMap,
