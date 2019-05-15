d09f8014dee5c5426de59a7464a5b17f97fdb194
lxgreen <alxgreen@gmail.com>
2019-05-15 16:45:58 +0300

    [plugins] ssr: true

--- packages/plugin-video/src/dynamic-video-viewer.js
+++ packages/plugin-video/src/dynamic-video-viewer.js
@@ -3,5 +3,5 @@ import loadable from '@loadable/component';
 
 export default loadable(() => import('./video-viewer.jsx'), {
   fallback: <div>Loading...</div>,
-  ssr: false, //TODO: add SSR support
+  ssr: true, //TODO: add SSR support
 });
