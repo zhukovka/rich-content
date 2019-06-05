f84844ecbbc6a0420705c6d6eec473f9e3646eff
Jonathan Bachman <jonathanb@wix.com>
2019-06-05 15:50:24 +0300

    [example] fix(html plugin config): use correct hostname

--- examples/main/src/utils.js
+++ examples/main/src/utils.js
@@ -37,3 +37,12 @@ export const getStateFromObject = obj => {
   const editorState = EditorState.createWithContent(convertFromRaw(normalizedState));
   return { editorState, viewerState: normalizedState };
 };
+
+export const getHostname = () => {
+  if (!window) {
+    return null;
+  }
+
+  const { hostname, port } = window.location;
+  return port ? `${hostname}:${port}` : hostname;
+};
