6f60b242394ff175b8137da65f3da1b01406314c
lxgreen <alxgreen@gmail.com>
2019-06-11 15:49:27 +0300

    [viewer] refactor(convert-content-state) kebabToCamel util moved to text-utils

--- packages/viewer/src/utils/textUtils.js
+++ packages/viewer/src/utils/textUtils.js
@@ -1,3 +1,11 @@
 import direction from 'direction';
 
 export const getTextDirection = text => direction(text);
+
+const kebabToCamel = s => s.replace(/-([a-z])/, (_, p1) => p1.toUpperCase());
+
+export const kebabToCamelObjectKeys = (obj = {}) =>
+  Object.keys(obj).reduce((result, key) => {
+    result[kebabToCamel(key)] = obj[key];
+    return result;
+  }, {});
