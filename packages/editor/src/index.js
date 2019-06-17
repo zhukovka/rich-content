b351bf165e1fe95d0a695be2f01b3bc9a262fd46
Jonathan Bachman <jonathanb@wix.com>
2019-06-17 17:42:03 +0300

    [editor] revert(5183ea3)

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -3,7 +3,10 @@ import RichContentEditor from './RichContentEditor';
 import RichContentEditorModal from './RichContentEditor/RichContentEditorModal';
 import { CONTENT_STATE_VERSION } from 'wix-rich-content-common';
 
-const addVersion = (obj, VERSION) => ({ VERSION, ...obj });
+const addVersion = (obj, version) => {
+  obj.VERSION = version;
+  return obj;
+};
 
 const convertToRaw = ContentState => addVersion(toRaw(ContentState), CONTENT_STATE_VERSION); //todo get current version
 const convertFromRaw = rawState => addVersion(fromRaw(rawState), rawState.VERSION);
