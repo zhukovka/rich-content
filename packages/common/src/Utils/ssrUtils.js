10be06bcabae1ffd6e8711ea47da2805adb96fe7
shaulg <shaulg@wix.com>
2019-06-23 20:04:18 +0300

    [video] fix(react-player-wrapper) window is undefined on SSR

--- packages/common/src/Utils/ssrUtils.js
+++ packages/common/src/Utils/ssrUtils.js
@@ -1,9 +1,1 @@
-export const isWindowAvailable = () => {
-  try {
-    return typeof window !== 'undefined';
-  } catch (e) {
-    return false;
-  }
-};
-
-export const getWindow = () => (isWindowAvailable() ? window : {});
+export const isSSR = () => typeof window === 'undefined';
