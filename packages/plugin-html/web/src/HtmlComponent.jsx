68f53a18efe4bb3e3a76f06c4c85aba162b64d52
lxgreen <alxgreen@gmail.com>
2019-10-03 12:05:01 +0300

    [plugins] fix(plugin-viewer) ViewportRenderer removed

--- packages/plugin-html/web/src/HtmlComponent.jsx
+++ packages/plugin-html/web/src/HtmlComponent.jsx
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-  mergeStyles,
-  isValidUrl,
-  normalizeUrl,
-  validate,
-  Context,
-  ViewportRenderer,
-} from 'wix-rich-content-common';
+import { mergeStyles, isValidUrl, normalizeUrl, validate, Context } from 'wix-rich-content-common';
 
 import {
   SRC_TYPE_HTML,
@@ -73,30 +66,28 @@ class HtmlComponent extends Component {
     const readOnly = blockProps ? blockProps.readOnly : true;
 
     return (
-      <ViewportRenderer>
-        <div
-          className={this.styles.htmlComponent}
-          ref={ref => (this.element = ref)}
-          style={style}
-          data-hook="HtmlComponent"
-        >
-          {srcType === SRC_TYPE_HTML && src && (
-            <IframeHtml
-              key={SRC_TYPE_HTML}
-              tabIndex={readOnly ? -1 : 0}
-              html={src}
-              src={htmlIframeSrc}
-              onHeightChange={this.setHeight}
-            />
-          )}
+      <div
+        className={this.styles.htmlComponent}
+        ref={ref => (this.element = ref)}
+        style={style}
+        data-hook="HtmlComponent"
+      >
+        {srcType === SRC_TYPE_HTML && src && (
+          <IframeHtml
+            key={SRC_TYPE_HTML}
+            tabIndex={readOnly ? -1 : 0}
+            html={src}
+            src={htmlIframeSrc}
+            onHeightChange={this.setHeight}
+          />
+        )}
 
-          {srcType === SRC_TYPE_URL && isValidUrl(src) && (
-            <IframeUrl key={SRC_TYPE_URL} tabIndex={readOnly ? -1 : 0} src={normalizeUrl(src)} />
-          )}
+        {srcType === SRC_TYPE_URL && isValidUrl(src) && (
+          <IframeUrl key={SRC_TYPE_URL} tabIndex={readOnly ? -1 : 0} src={normalizeUrl(src)} />
+        )}
 
-          {!src && !isValidUrl(src) && <div className={this.styles.htmlComponent_placeholder} />}
-        </div>
-      </ViewportRenderer>
+        {!src && !isValidUrl(src) && <div className={this.styles.htmlComponent_placeholder} />}
+      </div>
     );
   }
 }
