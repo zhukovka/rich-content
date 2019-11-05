e859bd97ef73979416eacbf1271de70abdf3a6c5
Alex Greenstein <lxgreen@users.noreply.github.com>
2019-11-05 11:21:08 +0200

    * [editor] fix(draft.rtlignore) missing placeholder styles (#448)

--- examples/main/shared/editor/Editor.jsx
+++ examples/main/shared/editor/Editor.jsx
@@ -161,6 +161,7 @@ export default class Editor extends PureComponent {
           </div>
         )}
         <RichContentEditor
+          placeholder={'Add some text!'}
           ref={editor => (this.editor = editor)}
           onChange={this.handleChange}
           helpers={this.helpers}
