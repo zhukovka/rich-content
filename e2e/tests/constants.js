e053d20fa4c5f233e850b54e74acffa662f5e3ca
Matan Harsat <matanha@wix.com>
2019-11-06 17:52:03 +0200

    [e2e] infra(constants): removed edge browser testing configuration

--- e2e/tests/constants.js
+++ e2e/tests/constants.js
@@ -10,7 +10,6 @@ export const fixtures = [
 export const DEFAULT_DESKTOP_BROWSERS = [
   { width: 1440, height: 900, name: 'chrome' },
   { width: 1440, height: 900, name: 'firefox' },
-  { width: 1440, height: 900, name: 'edge' },
 ];
 
 export const DEFAULT_MOBILE_BROWSERS = [
