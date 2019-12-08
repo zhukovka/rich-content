a934e679eb1e47ae93f840e7a4f6a781f62b97df
shaulg <shaulg@wix.com>
2019-12-08 19:38:39 +0200

    [viewer-ssr] remove unneeded code

--- examples/viewer-ssr/src/components/App/App.js
+++ examples/viewer-ssr/src/components/App/App.js
@@ -27,17 +27,6 @@ import TestData from './TestData/initial-state';
 import theme from './theme/theme';
 import styles from './App.scss';
 
-const modalStyleDefaults = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
 const anchorTarget = '_top';
 const relValue = 'noreferrer';
 
