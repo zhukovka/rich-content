db3cc9c871856530055c0b1e6b743882746f5422
shaulg <shaulg@wix.com>
2019-12-12 20:35:54 +0200

    lint

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -1,3 +1,4 @@
+/*global Cypress, cy*/
 require('cypress-plugin-snapshots/commands');
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 addMatchImageSnapshotCommand();
@@ -96,7 +97,8 @@ Cypress.Commands.add('focusEditor', () => {
 });
 
 Cypress.on('window:before:load', win => {
-  delete win.IntersectionObserver;
+  // noinspection JSAnnotator
+  delete win.IntersectionObserver; // eslint-disable-line fp/no-delete
   win.IntersectionObserver = class IntersectionObserverMock {
     constructor(cb, options) {
       this.cb = cb;
@@ -358,9 +360,8 @@ Cypress.Commands.add('addVideoFromURI', () => {
 Cypress.Commands.add('addHtml', () => {
   cy.get(`[data-hook*=${HTML_PLUGIN.STATIC_TOOLBAR_BUTTON}][tabindex!=-1]`).click();
   cy.get(`[data-hook*=${PLUGIN_TOOLBAR_BUTTONS.EDIT}]`).click();
-  cy.get(
-    `[data-hook*=${HTML_PLUGIN.INPUT}]`
-  ).type(
+  cy.get(`[data-hook*=${HTML_PLUGIN.INPUT}]`).type(
+    // eslint-disable-next-line max-len
     '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">The updates, insights and stories of the engineering challenges we encounter, and our way of solving them. Subscribe to our fresh, monthly newsletter and get these goodies right to your e-mail:<a href="https://t.co/0ziRSJJAxK">https://t.co/0ziRSJJAxK</a> <a href="https://t.co/nTHlsG5z2a">pic.twitter.com/nTHlsG5z2a</a></p>&mdash; Wix Engineering (@WixEng) <a href="https://twitter.com/WixEng/status/1076810144774868992?ref_src=twsrc%5Etfw">December 23, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
     { delay: 0 }
   );
