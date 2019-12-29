1a98292a802622e5d4b79df93760565c71ceb224
lxgreen <alxgreen@gmail.com>
2019-12-30 00:27:13 +0200

    [common] feat(schemas) link-preview schema added

--- packages/common/web/src/lib/contentStateValidator.jsx
+++ packages/common/web/src/lib/contentStateValidator.jsx
@@ -7,6 +7,7 @@ import giphySchema from '../../statics/schemas/plugin-giphy.schema.json';
 import htmlSchema from '../../statics/schemas/plugin-html.schema.json';
 import imageSchema from '../../statics/schemas/plugin-image.schema.json';
 import linkSchema from '../../statics/schemas/plugin-link.schema.json';
+import linkPreviewSchema from '../../statics/schemas/plugin-link-preview.schema.json';
 import mapSchema from '../../statics/schemas/plugin-map.schema.json';
 import mentionsSchema from '../../statics/schemas/plugin-mentions.schema.json';
 import soundCloudSchema from '../../statics/schemas/plugin-sound-cloud.schema.json';
@@ -20,6 +21,7 @@ import {
   HTML_TYPE,
   IMAGE_TYPE,
   LINK_TYPE,
+  LINK_PREVIEW_TYPE,
   MAP_TYPE,
   MENTION_TYPE,
   SOUND_CLOUD_TYPE,
@@ -38,6 +40,7 @@ export const isValidEditorData = payload => {
     [BUTTON_TYPE]: buttonSchema,
     [HTML_TYPE]: htmlSchema,
     [LINK_TYPE]: linkSchema,
+    [LINK_PREVIEW_TYPE]: linkPreviewSchema,
     [GALLERY_TYPE]: gallerySchema,
     [MENTION_TYPE]: mentionsSchema,
   });
