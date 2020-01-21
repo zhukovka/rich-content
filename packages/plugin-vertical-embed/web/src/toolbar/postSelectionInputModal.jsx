cb6cd19dc89f5f8aa164544cf1952508769c257d
Jurij Mikelionis <jurijm@wix.com>
2020-01-21 15:30:00 +0200

    search and stuff

--- packages/plugin-vertical-embed/web/src/toolbar/postSelectionInputModal.jsx
+++ packages/plugin-vertical-embed/web/src/toolbar/postSelectionInputModal.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { CloseIcon, Button } from 'wix-rich-content-editor-common';
+import { CloseIcon, Button, TextInput } from 'wix-rich-content-editor-common';
 import { mergeStyles } from 'wix-rich-content-common';
 import styles from '../../statics/styles/post-selection-input-modal.scss';
 import ItemsListComponent from '../components/items-list-component';
@@ -12,7 +12,7 @@ export default class PostSelectionInputModal extends Component {
   };
   styles = mergeStyles({ styles, theme: this.props.theme });
 
-  afterOpenModal = () => { };
+  afterOpenModal = () => this.input.focus();
 
   onCloseRequested = () => {
     this.setState({ isOpen: false });
@@ -25,29 +25,46 @@ export default class PostSelectionInputModal extends Component {
     }
   };
 
-  getMappedPosts(posts) {
-    return posts.map(post => ({
-      imageUrl: `https://static.wixstatic.com/media/${post.coverImage.url}`,
-      subtitle: new Date(post.lastPublishedDate).toDateString(),
-      ...post,
-    }));
-  }
-
-  fetchPosts = () =>
-    fetch(`${this.props.blogAPIUrl}/posts?limit=20`, {
-      headers: {
-        Accept: 'application/json',
-        Authorization: this.props.instance,
-      },
-    })
-      .then(resp => (resp.ok ? resp.json() : {}))
-      .then(({ post }) => this.getMappedPosts(post))
-      .then(posts => this.setState({ posts }))
-      .catch(error => console.log('Error in fetching posts: ', error));
-
   componentDidMount() {
-    this.fetchPosts();
+    this.input.focus();
+    this.input.setSelectionRange(0, this.input.value.length);
+    this.props.fetchPosts().then(posts => this.setState({ posts }));
   }
+
+  handleKeyPress = () => {};
+
+  search = async e => {
+    const posts = await this.props.searchPosts(e.target.value);
+    this.setState({ posts });
+  };
+
+  currentAbortController = null;
+  onInputChange = async e => {
+    const { abortController, promise } = this.props.searchPosts(e.target.value);
+    if (this.currentAbortController) {
+      this.currentAbortController.abort();
+    }
+    this.currentAbortController = abortController;
+
+    try {
+      const posts = await promise;
+      this.setState({ posts });
+    } catch (e) {
+      if (e.name === 'AbortError') {
+        return;
+      }
+      throw e;
+    }
+  };
+
+  // TODO
+  onConfirm = () => {};
+
+  handleKeyPress = e => {
+    if (e.charCode === 13) {
+      this.onConfirm();
+    }
+  };
 
   render() {
     const { posts } = this.state;
@@ -64,14 +81,28 @@ export default class PostSelectionInputModal extends Component {
             />
           )}
           <h2 className={styles.verticalEmbedModalTitle}>{t('Select_Blog_Post_Title')}</h2>
-          {posts && posts.length && <ItemsListComponent items={posts}></ItemsListComponent>}
+          <TextInput
+            inputRef={ref => {
+              this.input = ref;
+            }}
+            onKeyPress={this.handleKeyPress}
+            onChange={this.onInputChange}
+            placeholder={t('VideoUploadModal_Input_Placeholder')}
+            theme={styles}
+            data-hook="videoUploadModalInput"
+          />
+          {posts && posts.length > 0 && <ItemsListComponent items={posts} />}
         </div>
 
         <div className={styles.actionButtonsContainer}>
-          <Button type="secondary" className={styles.actionButton} onClick={() => this.onCloseRequested()}>
+          <Button
+            type="secondary"
+            className={styles.actionButton}
+            onClick={() => this.onCloseRequested()}
+          >
             Cancel
           </Button>
-          <Button className={styles.actionButton} onClick={() => onConfirm()}>
+          <Button className={styles.actionButton} onClick={() => this.onConfirm()}>
             Choose
           </Button>
         </div>
@@ -91,6 +122,6 @@ PostSelectionInputModal.propTypes = {
   t: PropTypes.func,
   isMobile: PropTypes.bool,
   languageDir: PropTypes.string,
-  blogAPIUrl: PropTypes.string,
-  instance: PropTypes.string,
+  fetchPosts: PropTypes.func,
+  searchPosts: PropTypes.func,
 };
