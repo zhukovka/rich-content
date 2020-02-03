8853afd5ac09e03c11d5a63dde09198476665318
Noam Tayri <noamt@wix.com>
2020-02-03 12:56:18 +0200

    cleanup

--- packages/plugin-anchor/web/src/index.js
+++ packages/plugin-anchor/web/src/index.js
@@ -1,4 +1,3 @@
 export { createAnchorPlugin } from './createAnchorPlugin';
 export { ANCHOR_TYPE } from './types';
 export { default as AnchorViewer } from './AnchorViewer';
-export { Modals, ModalsMap } from './modals';
--- packages/plugin-anchor/web/src/toolbar/inline-buttons.jsx
+++ packages/plugin-anchor/web/src/toolbar/inline-buttons.jsx
@@ -2,15 +2,12 @@ import { BUTTONS } from 'wix-rich-content-editor-common';
 
 export default () => {
   return [
-    { keyName: 'sizeSmall', type: BUTTONS.SIZE_SMALL },
-    { keyName: 'sizeMedium', type: BUTTONS.SIZE_MEDIUM },
-    { keyName: 'separator1', type: BUTTONS.SEPARATOR, mobile: true },
     {
       keyName: 'anchor',
       type: BUTTONS.FLOATING_MODAL,
-      mobile: false,
+      mobile: true,
     },
-    { keyName: 'separator2', type: BUTTONS.SEPARATOR, mobile: true },
+    { keyName: 'separator1', type: BUTTONS.SEPARATOR, mobile: true },
     { keyName: 'delete', type: BUTTONS.DELETE, mobile: true },
   ];
 };
