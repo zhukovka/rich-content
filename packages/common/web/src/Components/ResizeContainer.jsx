d370e9ba8df9502e6aa48d9aa0b3ce2ae448143c
Sapir-Shamay <sapirs@wix.com>
2020-02-05 10:41:45 +0200

    add resize css

--- packages/common/web/src/Components/ResizeContainer.jsx
+++ packages/common/web/src/Components/ResizeContainer.jsx
@@ -2,28 +2,20 @@ import React from 'react';
 import styles from '../../statics/styles/resize-container.scss';
 import PropTypes from 'prop-types';
 
-const ResizeDot = () => (
-  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-    <g color="blue">
-      <circle r="4" cx="5" cy="5" stroke="currentColor" fill="none" strokeWidth="0.5" />
-    </g>
-  </svg>
-);
+// const ResizeDot = () => (
+//   <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+//     <g color="blue">
+//       <circle r="4" cx="5" cy="5" stroke="currentColor" fill="none" strokeWidth="0.5" />
+//     </g>
+//   </svg>
+// );
 
 export const ResizeContainer = ({ /*right, left, both,*/ children, ...containerProps }) => {
   return (
-    <div className={styles.container} {...containerProps}>
-      <div className={styles.resizeHandleL}>
-        <ResizeDot />
-        <ResizeDot />
-      </div>
-
-      <div className={styles.image}> {children} </div>
-
-      <div className={styles.resizeHandleR}>
-        <ResizeDot />
-        <ResizeDot />
-      </div>
+    <div {...containerProps}>
+      <div className={styles.resizeHandleR} />
+      <div className={styles.resizeHandleL} />
+      {children}
     </div>
   );
 };
