3ce4d5d575be565a9fdf31e17cb82d9591bfa480
Dana Rish <danari@wix.com>
2020-02-10 11:16:51 +0200

    clean index.js

--- packages/plugin-image/web/src/icons/index.js
+++ packages/plugin-image/web/src/icons/index.js
@@ -1,4 +1,3 @@
 export { default as InsertPluginIcon } from './InsertPluginIcon';
-export { default as MediaReplaceIcon } from './MediaReplaceIcon';
 export { default as ImageEditorIcon } from './ImageEditorIcon.svg';
 export { default as MoreIcon } from './MoreIcon';
