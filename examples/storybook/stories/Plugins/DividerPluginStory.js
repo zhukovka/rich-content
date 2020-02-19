04ca9d2c83761d3872041ce8b4a98e7290d82e11
Oded Soffrin <odeds@wix.com>
2020-02-19 21:25:00 +0200

    lint fix

--- examples/storybook/stories/Plugins/DividerPluginStory.js
+++ examples/storybook/stories/Plugins/DividerPluginStory.js
@@ -14,6 +14,7 @@ import { convertFromRaw, createWithContent } from 'wix-rich-content-editor';
 import dividerContentState from '../../../../e2e/tests/fixtures/divider.json';
 import DividerEditor from './DividerEditor';
 
+// eslint-disable-next-line no-undef
 const sourcecode = preval`
   const fs = require('fs')  
   module.exports = fs.readFileSync(__dirname + '/DividerEditor.js', 'utf8')
