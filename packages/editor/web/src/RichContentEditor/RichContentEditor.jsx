9956e91431dc30244830ee156096e9aba1eb0cdf
Elad Bogomolny <eladbo@wix.com>
2020-02-23 11:09:58 +0200

    remove blockrendermap

--- packages/editor/web/src/RichContentEditor/RichContentEditor.jsx
+++ packages/editor/web/src/RichContentEditor/RichContentEditor.jsx
@@ -11,7 +11,6 @@ import { createKeyBindingFn, initPluginKeyBindings } from './keyBindings';
 import handleKeyCommand from './handleKeyCommand';
 import handleReturnCommand from './handleReturnCommand';
 import blockStyleFn from './blockStyleFn';
-import getBlockRenderMap from './getBlockRenderMap';
 import { combineStyleFns } from './combineStyleFns';
 import { getStaticTextToolbarId } from './Toolbars/toolbar-id';
 import {
@@ -312,7 +311,6 @@ class RichContentEditor extends Component {
         handlePastedText={handlePastedText}
         plugins={this.plugins}
         blockStyleFn={blockStyleFn(theme, this.styleToClass)}
-        blockRenderMap={getBlockRenderMap(theme)}
         handleKeyCommand={handleKeyCommand(
           this.updateEditorState,
           this.getCustomCommandHandlers().commandHanders
