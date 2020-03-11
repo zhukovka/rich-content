346ec6ff085d77faddf4a659d4299718c6911ea9
Dana Rish <45333005+danarish4@users.noreply.github.com>
2020-03-11 09:42:44 +0200

    [plugins] New Plugin Generator script (#683)

---
+++ scripts/templates/atomic-plugin/web/src/index.js
@@ -0,0 +1,3 @@
+export { createYourPluginNamePlugin } from './createYourPluginNamePlugin';
+export { default as yourPluginNameViewer } from './yourPluginName-viewer';
+export { YOUR_PLUGIN_NAME_TYPE } from './types';
