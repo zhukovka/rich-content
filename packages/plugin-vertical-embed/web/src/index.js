153507b343c101bc62ce48e7f48978f2cf16b2f6
Dana Rish <danari@wix.com>
2020-03-17 17:45:52 +0200

    fix wix wrapper supporting

--- packages/plugin-vertical-embed/web/src/index.js
+++ packages/plugin-vertical-embed/web/src/index.js
@@ -1,3 +1,4 @@
 export { createVerticalEmbedPlugin } from './createVerticalEmbedPlugin';
 export { VERTICAL_EMBED_TYPE } from './constants';
 export { Modals, ModalsMap } from './modals';
+export { pluginVerticalEmbed } from './editor';
