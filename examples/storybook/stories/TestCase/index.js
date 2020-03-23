bf6ffe783d7b0528af35fd2959a7d622a00ab980
Oded Soffrin <odeds@wix.com>
2020-03-23 13:34:18 +0200

    [storybook] one liner demo

--- examples/storybook/stories/TestCase/index.js
+++ examples/storybook/stories/TestCase/index.js
@@ -4,8 +4,10 @@ import HTMLPluginStory from './HtmlPlugin.js';
 import BlogLefties from './BlogLefties.js';
 import ImageFloatSpacing from './ImageFloatSpacing.js';
 import MaxHeight from './MaxHeight';
+import OneLiner from './OneLiner';
 
 storiesOf('Test Cases')
+  .add('One-Liner', OneLiner)
   .add('Image Float Spacing', ImageFloatSpacing)
   .add('Blog Lefties', BlogLefties)
   .add('HTML Instagram Height', HTMLPluginStory)
