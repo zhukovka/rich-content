1bff7d65ace511328dd62ea6eceeb5ab4e389eef
Tal Levy <tall@wix.com>
2020-03-26 14:26:13 +0200

    design changes

--- examples/main/shared/viewer/Viewer.jsx
+++ examples/main/shared/viewer/Viewer.jsx
@@ -33,10 +33,6 @@ export default class Viewer extends PureComponent {
     const additionalConfig = { [GALLERY_TYPE]: { scrollingElement: scrollingElementFn } };
     this.pluginsConfig = Plugins.getConfig(additionalConfig);
   }
-  componentDidMount() {
-    window.addEventListener('resize', this.resized);
-  }
-  resized = () => console.log('wow');
 
   componentDidUpdate(prevProps) {
     if (prevProps.initialState !== this.props.initialState) {
