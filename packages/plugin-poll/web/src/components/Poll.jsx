63481cf8afcf85ed2d8f4f621397e39db4293ca5
Maksym Butsykin <maksymb@wix.com>
2020-03-30 01:19:16 +0300

    poll revert

--- packages/plugin-poll/web/src/components/Poll.jsx
+++ packages/plugin-poll/web/src/components/Poll.jsx
@@ -66,43 +66,44 @@ class PollComponent extends Component {
       >
         <PollHeader />
 
-        <FlipMove
-          disableAllAnimations={!rce.isViewMode}
+        <ul
           className={cls(styles.options, {
             [styles.list]: layout.type === LAYOUT.LIST,
             [styles.grid]: layout.type === LAYOUT.GRID,
           })}
         >
-          {this.getOptionList().map((option, i) => (
-            <div className={styles.option} key={rce.isViewMode ? option.id : option.localId || i}>
-              <PollOption
-                imageEnabled={layout.type === LAYOUT.GRID}
-                option={option}
-                update={this.handleOptionUpdate(option.localId)}
-                remove={this.handleOptionRemove(option.localId)}
-                removeEnabled={!rce.isViewMode && poll.options.length > 1}
-                vote={vote}
-                unvote={unvote}
-                poll={poll}
-                showResults={this.showResults()}
-              />
-              <VotedUsers
-                option={option}
-                siteMembers={siteMembers}
-                showResults={this.showResults()}
-                showVoters={poll.settings.votersDisplay}
-              />
-            </div>
-          ))}
+          <FlipMove typeName={null} disableAllAnimations={!rce.isViewMode}>
+            {this.getOptionList().map((option, i) => (
+              <li className={styles.option} key={rce.isViewMode ? option.id : option.localId || i}>
+                <PollOption
+                  imageEnabled={layout.type === LAYOUT.GRID}
+                  option={option}
+                  update={this.handleOptionUpdate(option.localId)}
+                  remove={this.handleOptionRemove(option.localId)}
+                  removeEnabled={!rce.isViewMode && poll.options.length > 1}
+                  vote={vote}
+                  unvote={unvote}
+                  poll={poll}
+                  showResults={this.showResults()}
+                />
+                <VotedUsers
+                  option={option}
+                  siteMembers={siteMembers}
+                  showResults={this.showResults()}
+                  showVoters={poll.settings.votersDisplay}
+                />
+              </li>
+            ))}
+          </FlipMove>
 
           {!rce.isViewMode && (
-            <div>
+            <li>
               <button onClick={addOption} className={styles.addOptionButton} style={design.option}>
                 {layout.type === LAYOUT.GRID ? <AddIcon /> : t('Poll_Editor_Option_AddOption')}
               </button>
-            </div>
+            </li>
           )}
-        </FlipMove>
+        </ul>
       </div>
     );
   }
