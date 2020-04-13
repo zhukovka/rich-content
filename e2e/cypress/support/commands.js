e33ce6de12a464ef5e53f1da57e4ae5eaf5cea20
Shaul Goral <shaulg@wix.com>
2020-04-14 00:52:42 +0300

    e2e (#885)

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -127,6 +127,18 @@ Cypress.Commands.add('focusEditor', () => {
   cy.getEditor().focus();
 });
 
+Cypress.Commands.add(
+  'typeAllAtOnce',
+  {
+    prevSubject: 'element',
+  },
+  ($subject, value) => {
+    const el = $subject[0];
+    el.value = value;
+    return cy.wrap($subject).type('t{backspace}');
+  }
+);
+
 Cypress.on('window:before:load', win => {
   // noinspection JSAnnotator
   delete win.IntersectionObserver; // eslint-disable-line fp/no-delete
@@ -422,10 +434,9 @@ Cypress.Commands.add('addHtml', () => {
   cy.get(`[data-hook*=${HTML_PLUGIN.INPUT}]`)
     .click()
     .clear();
-  cy.get(`[data-hook*=${HTML_PLUGIN.INPUT}]`).type(
+  cy.get(`[data-hook*=${HTML_PLUGIN.INPUT}]`).typeAllAtOnce(
     // eslint-disable-next-line max-len
-    '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">The updates, insights and stories of the engineering challenges we encounter, and our way of solving them. Subscribe to our fresh, monthly newsletter and get these goodies right to your e-mail:<a href="https://t.co/0ziRSJJAxK">https://t.co/0ziRSJJAxK</a> <a href="https://t.co/nTHlsG5z2a">pic.twitter.com/nTHlsG5z2a</a></p>&mdash; Wix Engineering (@WixEng) <a href="https://twitter.com/WixEng/status/1076810144774868992?ref_src=twsrc%5Etfw">December 23, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
-    { delay: 0 }
+    '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">The updates, insights and stories of the engineering challenges we encounter, and our way of solving them. Subscribe to our fresh, monthly newsletter and get these goodies right to your e-mail:<a href="https://t.co/0ziRSJJAxK">https://t.co/0ziRSJJAxK</a> <a href="https://t.co/nTHlsG5z2a">pic.twitter.com/nTHlsG5z2a</a></p>&mdash; Wix Engineering (@WixEng) <a href="https://twitter.com/WixEng/status/1076810144774868992?ref_src=twsrc%5Etfw">December 23, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
   );
   cy.get(`[data-hook*=${HTML_PLUGIN.UPDATE}]`).click();
 });
