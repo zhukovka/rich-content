0d8eb9ba6831743f4a3519fd873e70a65cd03929
Dana Rish <45333005+danarish4@users.noreply.github.com>
2020-04-22 16:13:20 +0300

    [link-preview] feat: Let the consumers decide which services the… (#949)
    * [link-preview] feat: Let the consumers decide which services they want to enable inline

--- examples/main/shared/editor/EditorPlugins.jsx
+++ examples/main/shared/editor/EditorPlugins.jsx
@@ -146,7 +146,7 @@ let userButtonBorderColors = [...buttonDefaultPalette];
 
 const getLinkPanelDropDownConfig = () => {
   const getItems = () => {
-    casual.define('item', function () {
+    casual.define('item', function() {
       return {
         value: casual.url,
         label: casual.catch_phrase,
@@ -272,7 +272,8 @@ const { event, booking, product } = verticalEmbedProviders;
 const { Instagram, Twitter, YouTube, TikTok } = LinkPreviewProviders;
 const config = {
   [LINK_PREVIEW_TYPE]: {
-    enableEmbed: true,
+    enableEmbed: true, // [Twitter, YouTube]
+    enableLinkPreview: true,
     fetchData: mockFetchUrlPreviewData(),
     exposeEmbedButtons: [Instagram, Twitter, YouTube, TikTok],
   },
