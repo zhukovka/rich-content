8d8976e3c1f17022e229cb9eb31447dea7063b23
Oded Soffrin <odeds@wix.com>
2020-04-22 10:23:35 +0300

    [storybook] shakey twitter

--- examples/storybook/stories/TestCase/index.js
+++ examples/storybook/stories/TestCase/index.js
@@ -9,10 +9,12 @@ import OneLiner from './OneLiner';
 import VeryBigPost from './VeryBigPost';
 import ViewerAnchors from './ViewerAnchors';
 import MobileView from './MobileViewStory';
+import ShakeyTwitter from './ShakeyTwitter';
 
 storiesOf('Test Cases')
   .add('Viewer Anchors', () => <ViewerAnchors />)
   .add('Mobile view', MobileView)
+  .add('Shakey Twitter', ShakeyTwitter)
   .add('Very Big Post', VeryBigPost)
   .add('One-Liner', OneLiner)
   .add('Image Float Spacing', ImageFloatSpacing)
