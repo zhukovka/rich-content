3e6017961a1c3fb3a770394d9397f142c48fb0a8
Shaul Goral <shaulg@wix.com>
2020-04-27 14:16:44 +0300

    image storybook (#968)

---
+++ examples/storybook/stories/Plugins/Image/ImagePluginStory.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  RichContentEditorBox,
+  RichContentViewerBox,
+  Section,
+  Page,
+} from '../../Components/StoryParts';
+
+import { convertFromRaw, createWithContent } from 'wix-rich-content-editor';
+
+import imageContentState from '../../../../../e2e/tests/fixtures/images.json';
+import ImageEditor from './ImageEditor';
+import editorSourcecode from '!!raw-loader!./ImageEditor.js';
+import ImageViewer from './ImageViewer';
+import viewerSourcecode from '!!raw-loader!./ImageViewer.js';
+import SyntaxHighlighter from '../../Components/SyntaxHighlighter';
+import { createEmpty } from 'wix-rich-content-editor/dist/lib/editorStateConversion';
+
+const editorState = createWithContent(convertFromRaw(imageContentState));
+
+const mockData = {
+  id: '8b72558253b2502b401bb46e5599f22a',
+  original_file_name: '8bb438_1b73a6b067b24175bd087e86613bd00c.jpg', //eslint-disable-line
+  file_name: '8bb438_1b73a6b067b24175bd087e86613bd00c.jpg', //eslint-disable-line
+  width: 1920,
+  height: 1000,
+};
+const onFilesChangeMap = {
+  mock: (files, updateEntity) => {
+    setTimeout(() => {
+      updateEntity({
+        data: mockData,
+        files,
+      });
+    }, 2000);
+  },
+  error: (files, updateEntity) => {
+    setTimeout(() => {
+      updateEntity({ data: [], error: { msg: 'file too large' } });
+    }, 2000);
+  },
+  errorNoUI: (files, updateEntity) => {
+    setTimeout(() => {
+      updateEntity({ data: [], error: { msg: 'file too large', displayMessage: false } });
+    }, 2000);
+  },
+};
+
+const ImagePluginStory = () => (
+  <Page title="Image Plugin">
+    <Section type={Section.Types.COMPARISON}>
+      <RichContentEditorBox sourcecode={editorSourcecode} contentState={imageContentState}>
+        <ImageEditor editorState={editorState} onFilesChange={onFilesChangeMap.mock} />
+      </RichContentEditorBox>
+      <RichContentViewerBox sourcecode={viewerSourcecode}>
+        <ImageViewer initialState={imageContentState} />
+      </RichContentViewerBox>
+    </Section>
+
+    <Section title="onFilesChange Error (with UI)">
+      <div>With Error Message:</div>
+      <SyntaxHighlighter
+        code={`onFilesChange = (files, updateEntity) => updateEntity({ data: [], error: { msg: 'file too large' } });`}
+      />
+      <RichContentEditorBox>
+        <ImageEditor editorState={createEmpty()} onFilesChange={onFilesChangeMap.error} />
+      </RichContentEditorBox>
+    </Section>
+    <Section title="onFilesChange Error (without UI)">
+      <div>Without Error Message</div>
+      <SyntaxHighlighter
+        code={`onFilesChange = (files, updateEntity) => updateEntity({ data: [], error: { msg: 'file too large', displayMessage: false} });`}
+      />
+      <RichContentEditorBox>
+        <ImageEditor editorState={createEmpty()} onFilesChange={onFilesChangeMap.errorNoUI} />
+      </RichContentEditorBox>
+    </Section>
+  </Page>
+);
+
+export default ImagePluginStory;
--- examples/storybook/stories/Plugins/index.js
+++ examples/storybook/stories/Plugins/index.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import { storiesOf } from '@storybook/react';
-import LinkPreviewStory from './LinkPreviewStory';
-import GalleryPlugin from './GalleryPlugin.js';
-import DividerPluginStory from './DividerPluginStory.js';
+import LinkPreviewStory from './LinkPreview';
+import GalleryPlugin from './GalleryPlugin';
+import DividerPluginStory from './Divider';
+import Image from './Image';
 
 storiesOf('Plugins')
-  .add('Divider Plugin', DividerPluginStory)
-  .add('Gallery Plugin', GalleryPlugin)
+  .add('Divider', DividerPluginStory)
+  .add('Image', Image)
+  .add('Gallery', GalleryPlugin)
   .add('Link Preview', LinkPreviewStory);
