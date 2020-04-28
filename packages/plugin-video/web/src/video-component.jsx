137b0a87066192c090d09e84b48010c604d46ea8
shaulg <shaulg@wix.com>
2020-04-28 09:00:03 +0300

    [video] fix default uploadMediaError

--- packages/plugin-video/web/src/video-component.jsx
+++ packages/plugin-video/web/src/video-component.jsx
@@ -45,7 +45,7 @@ class VideoComponent extends React.Component {
       componentState: { error },
     } = props;
     if (error) {
-      const errorMsg = error?.message || this.props.t('UploadMediaError');
+      const errorMsg = error?.message || props.t('UploadMediaError');
       return { errorMsg };
     } else if (error === false) {
       return { errorMsg: undefined };
