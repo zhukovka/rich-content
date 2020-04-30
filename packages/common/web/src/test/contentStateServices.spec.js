d99096bea1f23057ef94b70b93b24e13ad3636a0
maimonav <maimonav@post.bgu.ac.il>
2020-04-30 18:50:07 +0300

    [common] fix: implemented func, no use of draft (#984)

--- packages/common/web/src/test/contentStateServices.spec.js
+++ packages/common/web/src/test/contentStateServices.spec.js
@@ -1,30 +1,33 @@
 /* eslint-disable max-len */
 import { truncateContentState } from '../lib/contentStateServices';
-import { raw, emptyRaw, expectedRaw1, expectedRaw2 } from './index';
+import {
+  contentState,
+  contentState2,
+  emptyContentState,
+  expectedContentState1,
+  expectedContentState2,
+  expectedContentState3,
+} from './index';
 
 describe('Test content state services functions', () => {
-  it('case: index = 0, should return empty raw', () => {
-    const newRaw = truncateContentState(raw, 0);
-    expect(newRaw).toEqual(emptyRaw);
+  it('case: index = 0, should return empty contentState', () => {
+    const newContentState = truncateContentState(contentState, 0);
+    expect(newContentState).toEqual(emptyContentState);
   });
-
   it('case: valid index, should be equal ', () => {
-    const newRaw = truncateContentState(raw, 2);
-    expect(newRaw).toEqual(expectedRaw1);
+    const newContentState = truncateContentState(contentState, 2);
+    expect(newContentState).toEqual(expectedContentState1);
   });
-
   it('case: valid index, should be equal ', () => {
-    const newRaw = truncateContentState(raw, 5);
-    expect(newRaw).toEqual(expectedRaw2);
+    const newContentState = truncateContentState(contentState, 5);
+    expect(newContentState).toEqual(expectedContentState2);
   });
-
-  it('case: index out of bounds, should return the entered raw', () => {
-    const newRaw = truncateContentState(raw, 6);
-    expect(newRaw).toEqual(raw);
+  it('case: valid index with 2 entityRanges, should be equal ', () => {
+    const newContentState = truncateContentState(contentState2, 2);
+    expect(newContentState).toEqual(expectedContentState3);
   });
-
-  it('case: index out of bounds, should return the entered raw', () => {
-    const newRaw = truncateContentState(raw, 100);
-    expect(newRaw).toEqual(raw);
+  it('case: index is bigger than blocks length, should return the entered contentState', () => {
+    const newContentState = truncateContentState(contentState, 6);
+    expect(newContentState).toEqual(contentState);
   });
 });
