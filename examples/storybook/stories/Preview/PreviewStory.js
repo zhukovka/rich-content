c6ca0ccf80cfa00b0c02c0daa9649035020c6bd2
Oded Soffrin <odeds@wix.com>
2020-05-01 11:14:42 +0300

    with drop down

--- examples/storybook/stories/Preview/PreviewStory.js
+++ examples/storybook/stories/Preview/PreviewStory.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Dropdown } from 'wix-style-react';
+
 import {
   RichContentViewerBox,
   RichContentEditorBox,
@@ -12,33 +14,22 @@ import EditorWrapper from '../Components/EditorWrapper';
 import { debounce } from 'lodash';
 import * as Plugins from '../../../main/shared/preview/PreviewPlugins.jsx';
 
-import introState from '../../../../e2e/tests/fixtures/intro.json';
-import { VIDEO_TYPE, videoTypeMapper } from 'wix-rich-content-plugin-video/dist/module.viewer';
+import { videoTypeMapper } from 'wix-rich-content-plugin-video/dist/module.viewer';
 import { dividerTypeMapper } from 'wix-rich-content-plugin-divider/dist/module.viewer';
 import { htmlTypeMapper } from 'wix-rich-content-plugin-html/dist/module.viewer';
 import { soundCloudTypeMapper } from 'wix-rich-content-plugin-sound-cloud/dist/module.viewer';
-import { LINK_TYPE, linkTypeMapper } from 'wix-rich-content-plugin-link/dist/module.viewer';
-import {
-  LINK_PREVIEW_TYPE,
-  linkPreviewTypeMapper,
-} from 'wix-rich-content-plugin-link-preview/dist/module.viewer';
+import { linkTypeMapper } from 'wix-rich-content-plugin-link/dist/module.viewer';
+import { linkPreviewTypeMapper } from 'wix-rich-content-plugin-link-preview/dist/module.viewer';
 import { imageTypeMapper } from 'wix-rich-content-plugin-image/dist/module.viewer';
-import {
-  galleryTypeMapper,
-  GALLERY_TYPE,
-} from 'wix-rich-content-plugin-gallery/dist/module.viewer';
+import { galleryTypeMapper } from 'wix-rich-content-plugin-gallery/dist/module.viewer';
 import { mapTypeMapper } from 'wix-rich-content-plugin-map/dist/module.viewer';
-import { giphyTypeMapper, GIPHY_TYPE } from 'wix-rich-content-plugin-giphy/dist/module.viewer';
+import { giphyTypeMapper } from 'wix-rich-content-plugin-giphy/dist/module.viewer';
 import { buttonTypeMapper } from 'wix-rich-content-plugin-button/dist/module.viewer';
 import { HashtagDecorator } from 'wix-rich-content-plugin-hashtag/dist/module.viewer';
-import {
-  MENTION_TYPE,
-  mentionsTypeMapper,
-} from 'wix-rich-content-plugin-mentions/dist/module.viewer';
-import {
-  fileUploadTypeMapper,
-  FILE_UPLOAD_TYPE,
-} from 'wix-rich-content-plugin-file-upload/dist/module.viewer';
+import { mentionsTypeMapper } from 'wix-rich-content-plugin-mentions/dist/module.viewer';
+import { fileUploadTypeMapper } from 'wix-rich-content-plugin-file-upload/dist/module.viewer';
+
+import introState from '../../../../e2e/tests/fixtures/very-big-post.json';
 
 export const typeMappers = [
   videoTypeMapper,
@@ -58,12 +49,48 @@ export const typeMappers = [
 
 export default () => {
   const [contentState, setContentState] = useState(introState);
+  const [ruleIdx, chooseRule] = useState(false);
+
   const onChange = debounce(changedEditorState => {
     setContentState(convertToRaw(changedEditorState.getCurrentContent()));
   }, 200);
 
+  const transformations = [
+    new ContentStateTransformation({
+      _if: metadata => metadata.plain.length > 0,
+      _then: (metadata, preview) => preview.plain(metadata.plain[0]).readMore({ lines: 3 }),
+    }),
+    new ContentStateTransformation({
+      _if: metadata => metadata.images.length > 0,
+      _then: (metadata, preview) => preview.image({ mediaInfo: metadata.images[0] }).seeFullPost(),
+    }),
+    new ContentStateTransformation({
+      _if: metadata => metadata.plain.length > 0,
+      _then: (metadata, preview) => preview.plain(metadata.plain[0]).readMore({ lines: 1 }),
+    }).rule({
+      _if: metadata => metadata.images.length > 3,
+      _then: (metadata, preview) =>
+        preview
+          .gallery({ mediaInfo: metadata.images.slice(0, 3) })
+          .imageCounter({ counter: metadata.images.length - 3 }),
+    }),
+  ];
+
+  const transformationProps = {};
+  if (ruleIdx !== false) {
+    transformationProps.transformation = transformations[ruleIdx];
+  }
   return (
-    <Page title="Preview">
+    <Page title="Preview Rules">
+      <Dropdown
+        placeholder="default, choose to change"
+        onSelect={({ id }) => chooseRule(id)}
+        options={[
+          { id: 0, value: 'Text only, without expand' },
+          { id: 1, value: 'One image only, with See Full Post' },
+          { id: 2, value: 'Text and image, exapnd on click' },
+        ]}
+      />
       <Section type={Section.Types.COMPARISON}>
         <RichContentEditorBox>
           <EditorWrapper contentState={contentState} onChange={onChange} />
@@ -72,7 +99,9 @@ export default () => {
         <RichContentViewerBox>
           <RichContentPreview
             //   locale={this.props.locale}
+            key={Date.now()}
             helpers={{}}
+            {...transformationProps}
             typeMappers={Plugins.typeMappers}
             inlineStyleMappers={Plugins.getInlineStyleMappers(contentState)}
             decorators={Plugins.decorators}
