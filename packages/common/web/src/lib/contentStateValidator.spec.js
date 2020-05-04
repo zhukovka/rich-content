f5946aa4d39e21547ec41997b1e4d2cf893d80a6
Oded Soffrin <odeds@wix.com>
2020-05-05 00:10:37 +0300

    missing files

---
+++ packages/common/web/src/lib/contentStateValidator.spec.js
@@ -0,0 +1,47 @@
+import { isValidEditorData } from './contentStateValidator';
+
+describe('Content State Validator', () => {
+  it('should return invalid result', async () => {
+    const validation = isValidEditorData({ what: 'hey!' });
+    expect(validation.valid).toEqual(false);
+  });
+  it('should return valid result', async () => {
+    const validExample = {
+      blocks: [
+        {
+          key: '9fe2n',
+          text: 'asdasd',
+          type: 'unstyled',
+          depth: 1,
+          inlineStyleRanges: [],
+          entityRanges: [],
+          data: {},
+        },
+      ],
+      entityMap: {},
+      VERSION: '7.4.6',
+    };
+    const validation = isValidEditorData(validExample);
+    expect(validation.valid).toEqual(true);
+  });
+
+  it('should invalidate according to depth', async () => {
+    const invalidDepth = {
+      blocks: [
+        {
+          key: '9fe2n',
+          text: 'asdasd',
+          type: 'unstyled',
+          depth: 1234,
+          inlineStyleRanges: [],
+          entityRanges: [],
+          data: {},
+        },
+      ],
+      entityMap: {},
+      VERSION: '7.4.6',
+    };
+    const validation = isValidEditorData(invalidDepth);
+    expect(validation.valid).toEqual(false);
+  });
+});
