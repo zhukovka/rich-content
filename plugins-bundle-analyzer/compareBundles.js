5815524368225b371351a5dc2ca64b8a65f9bc44
Bar Boaron <barb@wix.com>
2020-05-05 19:22:48 +0300

    trigger actions

--- plugins-bundle-analyzer/compareBundles.js
+++ plugins-bundle-analyzer/compareBundles.js
@@ -55,9 +55,10 @@ const updateMessage = (messageType, key, oldSize, newSize) => {
 
 async function updatePRCommentAndConsole() {
   const pr_comment = generatePRComment();
-  const shoulsPRComment = process.env.WITH_PR_COMMENT;
   console.log(pr_comment);
-  shoulsPRComment && (await gitPRComment(pr_comment, 'Comparison bundleSizes'));
+  if(process.env.WITH_PR_COMMENT){
+  await gitPRComment(pr_comment, 'Comparison bundleSizes');
+  }
 
   if (grewDownMessage !== '' || newBundles !== '') {
     fs.writeFileSync(`bundlesSizesBaseline.json`, JSON.stringify(savingBundles, null, 2), 'utf8');
