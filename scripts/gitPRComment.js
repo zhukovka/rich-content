33bd5548ced69f4eb76028051b73af911233e0e0
Bar Boaron <barb@wix.com>
2020-05-05 17:12:19 +0300

    trigger actions

--- scripts/gitPRComment.js
+++ scripts/gitPRComment.js
@@ -5,32 +5,33 @@ async function gitPRComment(message, header) {
   const { REPO_TOKEN } = process.env;
   if (REPO_TOKEN) {
     const context = github.context;
-    const issue_number = context.payload.pull_request.number;
-
-    const octokit = new github.GitHub(REPO_TOKEN);
-    const response = await octokit.issues.listComments({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      issue_number,
-    });
-
-    const allComments = response.data;
-    const comment = allComments.find(
-      comment => comment.user.login === 'github-actions[bot]' && comment.body.includes(header)
-    );
-    if (comment) {
-      await octokit.issues.updateComment({
-        ...context.repo,
+    const issue_number = context.payload?.number;
+    if (issue_number) {
+      const octokit = new github.GitHub(REPO_TOKEN);
+      const response = await octokit.issues.listComments({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
         issue_number,
-        comment_id: comment.id,
-        body: message,
       });
-    } else {
-      await octokit.issues.createComment({
-        ...context.repo,
-        issue_number,
-        body: message,
-      });
+
+      const allComments = response.data;
+      const comment = allComments.find(
+        comment => comment.user.login === 'github-actions[bot]' && comment.body.includes(header)
+      );
+      if (comment) {
+        await octokit.issues.updateComment({
+          ...context.repo,
+          issue_number,
+          comment_id: comment.id,
+          body: message,
+        });
+      } else {
+        await octokit.issues.createComment({
+          ...context.repo,
+          issue_number,
+          body: message,
+        });
+      }
     }
   }
 }
