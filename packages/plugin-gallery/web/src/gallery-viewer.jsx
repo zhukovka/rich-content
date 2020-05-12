e0419bc51b03d72be67bb69608b7988ed48e87cb
Tal Levy <tall@wix.com>
2020-05-12 16:47:53 +0300

    [gallery] fix(gallery-viewer): only render expand button if expand mode is available and change cursor to pointer

--- packages/plugin-gallery/web/src/gallery-viewer.jsx
+++ packages/plugin-gallery/web/src/gallery-viewer.jsx
@@ -196,12 +196,17 @@ class GalleryViewer extends React.Component {
     ) : null;
   };
 
-  hoverElement = itemProps => (
-    <Fragment>
-      {this.renderExpandIcon(itemProps)}
-      {this.renderTitle(itemProps.title)}
-    </Fragment>
-  );
+  hoverElement = itemProps => {
+    const hasExpand = this.props.helpers?.onExpand;
+    return hasExpand ? (
+      <div className={this.styles.pointer}>
+        {this.renderExpandIcon(itemProps)}
+        {this.renderTitle(itemProps.title)}
+      </div>
+    ) : (
+      <Fragment>{this.renderTitle(itemProps.title)}</Fragment>
+    );
+  };
 
   handleContextMenu = e => this.props.disableRightClick && e.preventDefault();
 
