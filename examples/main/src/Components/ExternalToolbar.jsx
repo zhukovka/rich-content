f2fa56c59ba66235550e46b80fc789c0fecc9e7d
lxgreen <alxgreen@gmail.com>
2020-05-18 18:20:57 +0300

    [examples/main] fix(ExternalToolbar) linter

--- examples/main/src/Components/ExternalToolbar.jsx
+++ examples/main/src/Components/ExternalToolbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FileInput, Tooltip, TooltipHost } from 'wix-rich-content-editor-common';
+import { FileInput, Tooltip } from 'wix-rich-content-editor-common';
 import { withPluginButtons } from 'wix-rich-content-editor';
 import styles from './ExternalToolbar.scss';
 
@@ -40,6 +40,7 @@ class ExternalToolbar extends Component {
                 </FileInput>
               );
             }
+            return null;
           }
         )}
       </div>
