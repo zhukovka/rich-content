0e09d18ef2bbae67ab82c2995bb8c6a09bc51e82
lxgreen <alxgreen@gmail.com>
2020-05-19 16:31:24 +0300

    [line-spacing] feat(createLineSpacingToolbar) externalizedButtonProps added

--- packages/plugin-line-spacing/web/src/toolbar/createLineSpacingToolbar.js
+++ packages/plugin-line-spacing/web/src/toolbar/createLineSpacingToolbar.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import LineSpacingButton from './LineSpacingButton';
+import { LINE_SPACING_TYPE } from '../types';
+import { LineSpacingIcon } from '../icons';
+import Panel from './LineSpacingPanel';
 import { decorateComponentWithProps } from 'wix-rich-content-editor-common';
 
-export default settings => ({
+export default config => ({
   TextButtonMapper: () => ({
     LinsSpacing: {
-      component: decorateComponentWithProps(LineSpacingButton, settings),
+      component: decorateComponentWithProps(LineSpacingButton, config[LINE_SPACING_TYPE]),
       isMobile: true,
     },
   }),
+  externalizedButtonProps: [
+    {
+      modalElement: props => <Panel {...props} {...config} />,
+      isActive: () => false,
+      icon: config[LINE_SPACING_TYPE]?.toolbar?.icons?.InsertPluginButtonIcon || LineSpacingIcon,
+      tooltip: config.t('TextHighlightButton_Tooltip'),
+      label: '',
+      name: LINE_SPACING_TYPE,
+      buttonType: 'modal',
+    },
+  ],
 });
