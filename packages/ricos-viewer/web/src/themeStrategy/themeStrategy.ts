ec0261e257746123f85e102433766f85461130d9
Barackos <barakl@wix.com>
2020-05-31 09:07:10 +0300

    [ricos] fix(themeStrategy): internal css injection

--- packages/ricos-viewer/web/src/themeStrategy/themeStrategy.ts
+++ packages/ricos-viewer/web/src/themeStrategy/themeStrategy.ts
@@ -1,21 +1,38 @@
 import ThemeGenerator from './ThemeGenerator';
-import jss from 'jss';
+import jss, { SheetsRegistry, Classes } from 'jss';
 import preset from 'jss-preset-default';
 import { defaultTheme } from './defaults';
 
 jss.setup(preset());
+
+let theme: RicosCssOverride,
+  rawCss: string,
+  prevPalette: Palette | PalettePreset,
+  paletteClasses: Classes;
 
 export default function themeStrategy(
   isViewer: boolean,
   themeGeneratorFunctions?: ThemeGeneratorFunction[],
   palette?: Palette | PalettePreset,
   cssOverride?: RicosCssOverride
-): { theme: RicosCssOverride } {
-  let paletteTheme = {};
-  if (palette) {
-    const themeGenerator = new ThemeGenerator(isViewer, palette, themeGeneratorFunctions);
-    const styles = jss.createStyleSheet(themeGenerator.getStylesObject());
-    paletteTheme = styles.attach().classes;
+): { theme: RicosCssOverride; rawCss: string } {
+  const sheets = new SheetsRegistry();
+  if (prevPalette !== palette || !rawCss) {
+    if (palette) {
+      prevPalette = palette;
+      const themeGenerator = new ThemeGenerator(isViewer, palette, themeGeneratorFunctions);
+      const sheet = jss.createStyleSheet(themeGenerator.getStylesObject());
+      sheets.add(sheet);
+      paletteClasses = sheet.classes;
+      rawCss = sheets.toString();
+    } else {
+      paletteClasses = {};
+      rawCss = '';
+    }
   }
-  return { theme: { ...defaultTheme, ...paletteTheme, ...cssOverride } };
+  theme = { ...defaultTheme, ...paletteClasses, ...cssOverride };
+  return {
+    theme,
+    rawCss,
+  };
 }
