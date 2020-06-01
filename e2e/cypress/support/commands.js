6784c67437d93481f05829b608958aac2b7626bd
shaulg <shaulg@wix.com>
2020-06-01 14:50:51 +0300

    [cypress] fix loadRicosEditorAndViewer command

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -96,7 +96,9 @@ Cypress.Commands.add('loadEditorAndViewer', (fixtureName, config) =>
 Cypress.Commands.add('loadIsolatedEditorAndViewer', fixtureName =>
   run('rce-isolated', fixtureName)
 );
-Cypress.Commands.add('loadRicosEditorAndViewer', fixtureName => run('ricos', fixtureName));
+Cypress.Commands.add('loadRicosEditorAndViewer', (fixtureName, config) =>
+  run('ricos', fixtureName, config)
+);
 
 Cypress.Commands.add('loadTestAppOnSsr', (fixtureName, compName) => {
   cy.request(getUrl(compName, fixtureName))
