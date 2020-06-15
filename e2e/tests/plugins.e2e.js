5a8856a7c132e789d17cc76a4c9200a81810755a
Dana Rish <danari@wix.com>
2020-06-15 15:11:13 +0300

    [test] fix: fix adsense test

--- e2e/tests/plugins.e2e.js
+++ e2e/tests/plugins.e2e.js
@@ -45,32 +45,6 @@ describe('plugins', () => {
     });
   });
 
-  context('adsense', () => {
-    before(function() {
-      eyesOpen(this);
-    });
-
-    beforeEach('load editor', () => {
-      cy.switchToDesktop();
-    });
-
-    after(() => cy.eyesClose());
-
-    const testAppConfig = {
-      ...usePlugins(plugins.html),
-      ...usePluginsConfig({
-        'wix-draft-plugin-html': {
-          siteDomain: 'https://www.wix.com',
-          exposeButtons: ['html', 'adsense'],
-        },
-      }),
-    };
-    it('render adsense plugin toolbar', function() {
-      cy.loadRicosEditorAndViewer('empty', testAppConfig).openAdsensedModal();
-      cy.eyesCheckWindow(this.test.title);
-    });
-  });
-
   context('divider', () => {
     before(function() {
       eyesOpen(this);
@@ -469,6 +443,32 @@ describe('plugins', () => {
 
     it('Change headers - without dropDownOptions config', () => {
       testHeaders(usePlugins(plugins.headings));
+    });
+  });
+
+  context('adsense', () => {
+    before(function() {
+      eyesOpen(this);
+    });
+
+    beforeEach('load editor', () => {
+      cy.switchToDesktop();
+    });
+
+    after(() => cy.eyesClose());
+
+    const testAppConfig = {
+      ...usePlugins(plugins.html),
+      ...usePluginsConfig({
+        'wix-draft-plugin-html': {
+          siteDomain: 'https://www.wix.com',
+          exposeButtons: ['html', 'adsense'],
+        },
+      }),
+    };
+    it('render adsense plugin toolbar', function() {
+      cy.loadRicosEditorAndViewer('empty', testAppConfig).openAdsensedModal();
+      cy.eyesCheckWindow(this.test.title);
     });
   });
 });
