a8cea0ca315040a0ec9c5ecad24b0c2d368345d6
Dana Rish <45333005+danarish4@users.noreply.github.com>
2020-06-18 16:02:18 +0300

    [storybook]: add adsense plugin to html story

--- examples/storybook/stories/Plugins/HtmlPlugin/HtmlWithDomainStory.js
+++ examples/storybook/stories/Plugins/HtmlPlugin/HtmlWithDomainStory.js
@@ -2,12 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { RicosEditor } from 'ricos-editor';
 import { RichContentEditor } from 'wix-rich-content-editor';
-import { pluginHtml } from 'wix-rich-content-plugin-html';
+import { pluginHtml, htmlButtonsTypes } from 'wix-rich-content-plugin-html';
 import { pluginLinkPreview } from 'wix-rich-content-plugin-link-preview';
 
 const HtmlWithDomainStory = ({ contentState }) => {
+  const { html, adsense } = htmlButtonsTypes;
   return (
-    <RicosEditor plugins={[pluginHtml(), pluginLinkPreview()]} contentState={contentState}>
+    <RicosEditor
+      plugins={[
+        pluginHtml({ exposeButtons: [html, adsense], siteDomain: 'https://www.wix.com' }),
+        pluginLinkPreview(),
+      ]}
+      contentState={contentState}
+    >
       <RichContentEditor
         initialState={contentState}
         iframeSandboxDomain="https://richcontent-sttorybook.filesusr.com"
