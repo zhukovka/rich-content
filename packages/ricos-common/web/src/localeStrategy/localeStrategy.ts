cfec128946be4893997396a73637d4edbd4f8372
Oded Soffrin <odeds@wix.com>
2020-06-19 16:11:26 +0300

    [ricos-common] add webchunks name (#1262)
    hopefully it will resolve the flaky tests

--- packages/ricos-common/web/src/localeStrategy/localeStrategy.ts
+++ packages/ricos-common/web/src/localeStrategy/localeStrategy.ts
@@ -4,6 +4,7 @@ export default async function localeStrategy(locale = 'en') {
   }
   try {
     const localeResource = await import(
+      /* webpackChunkName: "messages_${locale}" */
       `wix-rich-content-common/dist/statics/locale/messages_${locale}.json`
     ).then(res => res.default);
     return { locale, localeResource };
