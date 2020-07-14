423f47ec8376dc0c319dc25d241c5b17051be307
Shaul Goral <shaulg@wix.com>
2020-07-14 20:03:34 +0300

    [image] (image-settings) add label for link position description (#1335)

--- packages/plugin-image/web/src/toolbar/image-settings.js
+++ packages/plugin-image/web/src/toolbar/image-settings.js
@@ -27,6 +27,7 @@ class ImageSettings extends Component {
     this.altLabel = t('ImageSettings_Alt_Label');
     this.altTooltip = 'ImageSettings_Alt_Label_Tooltip';
     this.altInputPlaceholder = t('ImageSettings_Alt_Input_Placeholder');
+    this.linkRedirectTextLabel = t('ImageSettings_Link_Label');
     this.linkRedirectText = t('ImageSettings_Link_RedirectToToolbar');
   }
 
@@ -168,7 +169,8 @@ class ImageSettings extends Component {
             className={this.styles.imageSettingsSection}
             ariaProps={{ 'aria-label': 'link redirect explanation', role: 'region' }}
           >
-            {this.linkRedirectText}
+            <div className={this.styles.imageSettingsLabel}>{this.linkRedirectTextLabel}</div>
+            <div>{this.linkRedirectText}</div>
           </SettingsSection>
         </div>
         {isMobile ? null : (
