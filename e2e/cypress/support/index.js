f426048bde5fe245efe4e391f56362ff0bd81e0d
Shaul Goral <shaulg@wix.com>
2020-08-15 13:14:51 +0300

    [e2e] fix Applitools Antialiasing HelveticaBug (#1434)

--- e2e/cypress/support/index.js
+++ e2e/cypress/support/index.js
@@ -8,3 +8,14 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false;
 });
+
+Cypress.Commands.overwrite('eyesCheckWindow', (originalFn, config = {}) => {
+  const obj = typeof config === 'string' ? { tag: config } : config;
+  originalFn({
+    ...obj,
+    scriptHooks: {
+      beforeCaptureScreenshot:
+        "[...document.styleSheets].forEach(s => [...s.rules].forEach(r => r.style && r.style.getPropertyValue('font-family') && r.style.setProperty('font-family', r.style.getPropertyValue('font-family').split(',').map(f => f.trim() === 'Helvetica' ? 'sans-serif' : f).join(','))))",
+    },
+  });
+});
