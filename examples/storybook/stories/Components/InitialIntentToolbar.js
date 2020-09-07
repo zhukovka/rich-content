cc695273eacbbe5a5fa288a6b6b5e91e7ec73075
Barackos <barakl@wix.com>
2020-09-07 16:57:25 +0300

    [storybook] fix(InitialIntentToolbar): FileInput

--- examples/storybook/stories/Components/InitialIntentToolbar.js
+++ examples/storybook/stories/Components/InitialIntentToolbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { pick } from 'lodash';
-import { FileInput, BUTTON_TYPES, INSERT_PLUGIN_BUTTONS } from 'wix-rich-content-editor-common';
+import { FileInput } from 'wix-rich-content-plugin-commons';
+import { BUTTON_TYPES, INSERT_PLUGIN_BUTTONS } from 'wix-rich-content-editor-common';
 import Tooltip from 'wix-rich-content-common/dist/lib/Tooltip.cjs.jsx';
 import PhotoCamera from 'wix-ui-icons-common/PhotoCamera';
 import VideoCamera from 'wix-ui-icons-common/VideoCamera';
@@ -65,7 +65,7 @@ class InitialIntentToolbar extends Component {
                 );
               }
               return null;
-            },
+            }
           )}
       </div>
     );
